0fa538e19dcd4bf5a99659258bdaebab01a5ecc5
Maureen Kelly <mkelly>
2009-05-05 21:37:47 +0000

    added date created and created by processing to AsaTaxon

--- Specify/src/edu/harvard/huh/asa/AsaTaxon.java
+++ Specify/src/edu/harvard/huh/asa/AsaTaxon.java
@@ -1,8 +1,25 @@
 package edu.harvard.huh.asa;
 
+import java.util.Date;
+
 public class AsaTaxon {
 	public static enum TYPE { Algae, Diatoms, FungiLichens, Hepatics, Monera, Mosses, Vascular };
 
+    // from st_lookup category 130
+    String NON_CITES_TYPE = "[none]";
+    String CITES_I_TYPE = "CITES I";
+    String CITES_II_TYPE = "CITES II";
+    String CITES_III_TYPE = "CITES III";
+
+    // from st_lookup category 140
+    String ALGAE_TYPE = "Algae";
+    String DIATOMS_TYPE = "Diatoms";
+    String FUNGI_TYPE = "Fungi & Lichens";
+    String HEPATICS_TYPE = "Hepatics";
+    String MONERA_TYPE = "Monera";
+    String MOSSES_TYPE = "Mosses";
+    String VASCULAR_TYPE = "Vascular plants";
+    
 	private Integer id;
 	private  String author;
 	private  String name;
@@ -11,9 +28,11 @@ public class AsaTaxon {
 	private Integer parentId;
 	private  String rank;
 	private  String remarks;
+	private Integer createdById;
+	private    Date dateCreated;
 	
 	public Integer getId() { return id; }
-
+	
 	public String getAuthor() { return author; }
 	
 	public String getName() { return name; }
@@ -27,6 +46,10 @@ public class AsaTaxon {
 	public String getRank() { return rank; }
 	
 	public String getRemarks() { return remarks; }
+	
+	public Integer getCreatedById() { return createdById; }
+	
+	public Date getDateCreated() { return dateCreated; }
 	
 	public void setId(Integer id) { this.id = id; }
 	
@@ -43,4 +66,8 @@ public class AsaTaxon {
 	public void setRank(String rank) { this.rank = rank; }
 	
 	public void setRemarks(String remarks) { this.remarks = remarks; }
+	
+	public void setCreatedById(Integer createdById) { this.createdById = createdById; }
+	
+	public void setDateCreated(Date dateCreated) { this.dateCreated = dateCreated; }
 }
