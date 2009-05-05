9ba3ab280759852170b964a1dbba9762b9aa5b46
Maureen Kelly <mkelly>
2009-05-05 21:39:54 +0000

    added synonimization of variant names, implemented parsing and sql writing

--- Specify/src/edu/harvard/huh/asa/GeoUnit.java
+++ Specify/src/edu/harvard/huh/asa/GeoUnit.java
@@ -1,16 +1,23 @@
 package edu.harvard.huh.asa;
 
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
 public class GeoUnit {
 
 	private Integer id;
 	private Integer parentId;
-	private  String name;
-	private  String abbreviation;
-	private  String isoCode;
-	private  String vernacularName;
-	private  String qualifiedName;
-	private  String rank;
-	private  String remarks;
+	private      String name;
+	private      String abbreviation;
+	private      String isoCode;
+	private      String vernacularName;
+	private      String displayQualifier;
+	private      String rank;
+	private      String remarks;
+	private     Integer createdById;
+	private        Date dateCreated;
+	private Set<String> variantNames = new HashSet<String>();
 	
 	public Integer getId() { return id; }
 	
@@ -24,12 +31,18 @@ public class GeoUnit {
 	
 	public String getVernacularName() { return vernacularName; }
 	
-	public String getQualifiedName() { return qualifiedName; }
+	public String getDisplayQualifier() { return displayQualifier; }
 	
 	public String getRank() { return rank; }
 	
 	public String getRemarks() { return remarks; }
+
+	public Integer getCreatedById() { return createdById; }
+	    
+	public Date getDateCreated() { return dateCreated; }
 	
+	public Set<String> getVariantNames() { return variantNames; }
+	    
 	public void setId(Integer id) { this.id = id; }
 	
 	public void setParentId(Integer parentId) { this.parentId = parentId; }
@@ -38,7 +51,7 @@ public class GeoUnit {
 	
 	public void setVernacularName(String vernacularName) { this.vernacularName = vernacularName; }
 	
-	public void setQualifiedname(String qualifiedName) { this.qualifiedName = qualifiedName; }
+	public void setDisplayQualifier(String displayQualifier) { this.displayQualifier = displayQualifier; }
 	
 	public void setAbbreviation(String abbreviation) { this.abbreviation = abbreviation; }
 	
@@ -47,4 +60,10 @@ public class GeoUnit {
 	public void setRank(String rank) { this.rank = rank; }
 	
 	public void setRemarks(String remarks) { this.remarks = remarks; }
+	
+	public void setCreatedById(Integer createdById) { this.createdById = createdById; }
+	    
+	public void setDateCreated(Date dateCreated) { this.dateCreated = dateCreated; }
+	
+	public void addVariantName(String variantName) { this.variantNames.add(variantName); }
 }
