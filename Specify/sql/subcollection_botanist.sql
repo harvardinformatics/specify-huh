2cc4107a8464a3a956ed922d08b3a809afa67727
Maureen Kelly <mkelly>
2009-05-20 21:32:03 +0000

    updated subcollection_botanist sql

--- Specify/sql/subcollection_botanist.sql
+++ Specify/sql/subcollection_botanist.sql
@@ -10,39 +10,71 @@ from
        (select id,
                taxon_group_id,
                author,
-               substr(author, 0, instr(author, '.')-1) as author_match,
+               lower(decode(instr(author, '.'),
+                            0,
+                            decode(instr(author, ','),
+                                   0,
+                                   decode(instr(author, '('),
+                                          0,
+                                          author,
+                                          substr(author, 0, instr(author, '(')-2)
+                                   ),
+                                  substr(author, 0, instr(author, ',')+2)
+                            ),
+                           substr(author, 0, instr(author, '.')-1)
+                     )
+               ) as author_match,
                name
           from subcollection
-         where length(author) > 4 and instr(author, '.') > 4
+         where length(author) > 4
        ) s,
        (select id, start_year, end_year, dates_type_id from botanist where team_flag=0) b,
-       (select distinct botanist_id, name from botanist_name where type_id in (110101)) bn
+       (select distinct botanist_id, lower(name) as name from botanist_name where type_id in (110101)) bn
 
 where
-      length(bn.name) >= length(s.author_match) and
+      length(s.author_match) > 4 and length(bn.name) >= length(s.author_match) and
       substr(bn.name, 0, length(s.author_match)) = s.author_match and
       bn.botanist_id=b.id
 
 order by s.id
 
 /*
-select s.id,
-       (select fullname from taxon where id=s.taxon_group_id) as taxon,
-       regexp_replace(s.name, '[[:space:]]+', ' ') as name,
-       regexp_replace(s.author, '[[:space:]]+', ' ') as author
+select sub.id,
+       (select fullname from taxon where id=sub.taxon_group_id) as taxon,
+       regexp_replace(sub.name, '[[:space:]]+', ' ') as name,
+       regexp_replace(sub.author, '[[:space:]]+', ' ') as author
 from
-     subcollection s
+     subcollection sub
 where
-     s.id not in (
-
-     select sub.id
-       from
-           (select id, author from subcollection where author is not null) sub,
-           (select id from botanist where team_flag=0) b,
-           (select distinct botanist_id, name from botanist_name where type_id in (110101)) bn
-
-      where length(s.author) > 4 and instr(s.author, '.') > 4 and length(bn.name) >= length(s.author) and
-            substr(s.author, 0, instr(s.author, '.')-1) = substr(bn.name, 0, instr(s.author, '.')-1) and
-            bn.botanist_id=b.id
-     )
+     sub.id not in (
+        select s.id
+          from
+              (select id,
+                      taxon_group_id,
+                      author,
+                      lower(decode(instr(author, '.'),
+                                   0,
+                                   decode(instr(author, ','),
+                                          0,
+                                          decode(instr(author, '('),
+                                                 0,
+                                                 author,
+                                                 substr(author, 0, instr(author, '(')-2)
+                                          ),
+                                         substr(author, 0, instr(author, ',')+2)
+                                   ),
+                                  substr(author, 0, instr(author, '.')-1)
+                            )
+                      ) as author_match,
+                      name
+                 from subcollection
+                where length(author) > 4
+              ) s,
+              (select id, start_year, end_year, dates_type_id from botanist where team_flag=0) b,
+              (select distinct botanist_id, lower(name) as name from botanist_name where type_id in (110101)) bn
+       where
+              length(s.author_match) > 4 and length(bn.name) >= length(s.author_match) and
+              substr(bn.name, 0, length(s.author_match)) = s.author_match and
+              bn.botanist_id=b.id
+    )
 */
