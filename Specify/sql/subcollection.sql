3a420e0d68c1316e30aa184e14a016080fe81d0e
Maureen Kelly <mkelly>
2009-05-20 17:40:48 +0000

    removed botanist_id from subcollection sql; will normalize authors externally

--- Specify/sql/subcollection.sql
+++ Specify/sql/subcollection.sql
@@ -3,7 +3,7 @@ select s.id,
        s.taxon_group_id,
        regexp_replace(s.name, '[[:space:]]+', ' ') as name,
        regexp_replace(s.author, '[[:space:]]+', ' ') as author,
-       bn.botanist_id,
+       /* bn.botanist_id, */
        regexp_replace(s.specimen_count, '[[:space:]]+', ' ') as specimen_count,
        regexp_replace(s.location, '[[:space:]]+', ' ') as location,
        regexp_replace(s.cabinet, '[[:space:]]+', ' ') as cabinet,
@@ -11,9 +11,9 @@ select s.id,
        to_char(s.create_date, 'YYYY-MM-DD HH24:MI:SS') as date_created,
        regexp_replace(s.remarks, '[[:space:]]+', ' ') as remarks
 from
-       subcollection s,
-       (select distinct botanist_id, name from botanist_name where type_id in (110102,110105)) bn
+       subcollection s
+       /* (select distinct botanist_id, name from botanist_name where type_id in (110102,110105)) bn */
 
-where s.author=bn.name(+)
+/* where s.author=bn.name(+) */
 
 order by s.id
