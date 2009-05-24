6548f5591a4abd1ebae0b4ccb2f4d4369ea3fd6a
Maureen Kelly <mkelly>
2009-05-24 18:12:09 +0000

    fixed minor errors in transaction.sql

--- Specify/sql/transaction.sql
+++ Specify/sql/transaction.sql
@@ -6,17 +6,18 @@ select t.id,
        (select name from st_lookup where id=t.purpose_id) as purpose,
        t.affiliate_id,
        (select name from st_lookup where id=t.user_type_id) as user_type,
-       decode(t.acknowledged_flag, 1 'true', '') as is_acknowledged,
+       decode(t.acknowledged_flag, 1, 'true', '') as is_acknowledged,
        to_char(t.open_date, 'YYYY-MM-DD HH24:MI:SS') as date_opened,
        to_char(t.close_date, 'YYYY-MM-DD HH24:MI:SS') as date_closed,
-       regexp_replace(t.transaction_no, '[[:space:]]', ' ') as transaction_no,
-       regexp_replace(t.for_use_by, '[[:space:]]', ' ') as for_use_by,
-       regexp_replace(t.box_count, '[[:space:]]', ' ') as box_count,
-       regexp_replace(t.description, '[[:space:]]', ' ') as description
-       regexp_replace(t.remarks, '[[:space:]]', ' ') as remarks,
+       regexp_replace(t.transaction_no, '[[:space:]]+', ' ') as transaction_no,
+       regexp_replace(t.for_use_by, '[[:space:]]+', ' ') as for_use_by,
+       regexp_replace(t.box_count, '[[:space:]]+', ' ') as box_count,
+       regexp_replace(t.description, '[[:space:]]+', ' ') as description,
+       regexp_replace(t.remarks, '[[:space:]]+', ' ') as remarks,
        t.created_by_id,
        to_char(t.create_date, 'YYYY-MM-DD HH24:MI:SS') as date_created,
-       to_char(select min(d.date_due) from due_date where id=t.id) as original_due_date,
-       to_char(select max(d.date_due) from due_date where id=t.id) as current_due_date
+       to_char((select min(date_due) from due_date where id=t.id)) as original_due_date,
+       to_char((select max(date_due) from due_date where id=t.id)) as current_due_date
+
 
 from herb_transaction t
