c749752f729668a6673384394dff75d1643c5ed0
Maureen Kelly <mkelly>
2009-05-28 22:42:32 +0000

    omitted alias name on some fields of in_geo_batch.sql; fixed

--- Specify/sql/in_geo_batch.sql
+++ Specify/sql/in_geo_batch.sql
@@ -2,12 +2,12 @@ select igb.id,
        igb.herb_transaction_id,
        (select name from st_lookup where id=t.type_id) as type,
        (select name from geo_name where geo_unit_id=igb.geo_region_id and type_id=110701) as src_geography,
-       item_count,
-       type_count,
-       non_specimen_count,
-       discard_count,
-       return_count,
-       cost
+       igb.item_count,
+       igb.type_count,
+       igb.non_specimen_count,
+       igb.discard_count,
+       igb.return_count,
+       igb.cost
 
 from in_geo_batch igb,
      herb_transaction t
