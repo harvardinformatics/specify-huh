dc364dc00b3735cd6aa8ae05e0dce8c3d8767878
Maureen Kelly <mkelly>
2009-06-25 00:06:25 +0000

    not using xml for creating nav box items; added rough outline of processing of commands for adding/deleting new subscriptions to nav box ui; hard-coded some other subscriptions

--- Specify/src/edu/ku/brc/specify/tasks/FilteredPushTask.java
+++ Specify/src/edu/ku/brc/specify/tasks/FilteredPushTask.java
@@ -23,11 +23,15 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Vector;
 
+import javax.swing.ImageIcon;
+import javax.swing.SwingUtilities;
+
 import org.apache.log4j.Logger;
 import org.dom4j.Element;
 
 import edu.ku.brc.af.core.AppContextMgr;
 import edu.ku.brc.af.core.ContextMgr;
+import edu.ku.brc.af.core.DroppableNavBox;
 import edu.ku.brc.af.core.NavBox;
 import edu.ku.brc.af.core.NavBoxButton;
 import edu.ku.brc.af.core.NavBoxIFace;
@@ -36,13 +40,26 @@ import edu.ku.brc.af.core.NavBoxMgr;
 import edu.ku.brc.af.core.SubPaneIFace;
 import edu.ku.brc.af.core.SubPaneMgr;
 import edu.ku.brc.af.core.ToolBarItemDesc;
+import edu.ku.brc.af.core.db.DBTableIdMgr;
+import edu.ku.brc.af.core.db.DBTableInfo;
 import edu.ku.brc.af.tasks.subpane.BaseSubPane;
 import edu.ku.brc.af.tasks.subpane.FilteredPushPane;
 import edu.ku.brc.af.tasks.subpane.SimpleDescPane;
+import edu.ku.brc.dbsupport.RecordSetIFace;
 import edu.ku.brc.services.filteredpush.FilteredPushMgr;
+import edu.ku.brc.specify.datamodel.RecordSet;
+import edu.ku.brc.specify.datamodel.SpQuery;
+import edu.ku.brc.specify.tasks.QueryTask.EditQueryWorker;
+import edu.ku.brc.specify.tools.schemalocale.SchemaLocalizerDlg;
+import edu.ku.brc.specify.ui.treetables.TreeDefinitionEditor;
+import edu.ku.brc.ui.CommandAction;
 import edu.ku.brc.ui.CommandDispatcher;
+import edu.ku.brc.ui.DataFlavorTableExt;
 import edu.ku.brc.ui.IconManager;
+import edu.ku.brc.ui.RolloverCommand;
 import edu.ku.brc.ui.ToolBarDropDownBtn;
+import edu.ku.brc.ui.dnd.FpPublish;
+import edu.ku.brc.ui.dnd.Trash;
 
 public class FilteredPushTask extends BaseTask
 {
@@ -50,10 +67,13 @@ public class FilteredPushTask extends BaseTask
     private static final Logger log  = Logger.getLogger(FilteredPushTask.class);
     
     public static final String FILTEREDPUSH  = "FilteredPush";
-    
-    private Element panelDOM;
+    public static final String NEW_SUBSCRIP  = "NewSubscription";
+    public static final String DEL_SUBSCRIP  = "DeleteSubscription";
     private SubPaneIFace fpPane;
     
+    private String subscripsBoxTitle = getResourceString("fp_subscrips_title");
+    protected NavBox subscripsNavBox;
+
     // When requestedContext(Taskable) is called on ContextMgr, it calls NavBoxMgr.register(Taskable).
     // In that method, this class's getNavBoxes() method is called, which is where this item gets
     // populated.
@@ -77,47 +97,30 @@ public class FilteredPushTask extends BaseTask
             
             extendedNavBoxes.clear();
             
-            try
-            {
-                panelDOM = AppContextMgr.getInstance().getResourceAsDOM("FilteredPushNavPanel");   // contains a description of the NavBoxes //$NON-NLS-1$
-                if (panelDOM == null)
-                {
-                    log.error("Couldn't load FilteredPushNavPanel"); //$NON-NLS-1$
-                    return;
-                }
-            } catch (Exception ex)
-            {
-                edu.ku.brc.af.core.UsageTracker.incrHandledUsageCount();
-                edu.ku.brc.exceptions.ExceptionTracker.getInstance().capture(FilteredPushTask.class, ex);
-                log.error("Couldn't load `FilteredPushNavPanel` " +ex); //$NON-NLS-1$
-            }
-    
-            // Process the NavBox Panel and create all the commands
-            // XXX This needs to be made generic so everyone can use it
-            //
-            List<?> boxes = panelDOM.selectNodes("/boxes/box"); //$NON-NLS-1$
-            for ( Iterator<?> boxIter = boxes.iterator(); boxIter.hasNext(); )
-            {
-                Element box = (Element) boxIter.next();
-                NavBox navBox = new NavBox(getResourceString(box.attributeValue("title"))); //$NON-NLS-1$
+            subscripsNavBox = new DroppableNavBox(subscripsBoxTitle, QueryTask.QUERY_FLAVOR, FILTEREDPUSH, NEW_SUBSCRIP);
 
-                List<?> items = box.selectNodes("item"); //$NON-NLS-1$
-                for ( Iterator<?> itemIter = items.iterator(); itemIter.hasNext(); )
-                {
-                    Element item = (Element) itemIter.next();
-                    String itemName  = item.attributeValue("name"); //$NON-NLS-1$
-                    String itemTitle = item.attributeValue("title"); //$NON-NLS-1$
-                    String itemIcon     = item.attributeValue("icon"); //$NON-NLS-1$
-                    
-                    ActionListener action = new DisplayAction(itemName);
-                    NavBoxItemIFace btn = NavBox.createBtn(getResourceString(itemTitle), itemIcon, IconManager.STD_ICON_SIZE, action);
+            String newSubscripTitle = getResourceString("fp_new_subscrip_title");
+            String newSubscripIcon = "EMail";
+            ActionListener defaultAction = new DisplayAction(subscripsBoxTitle);
 
-                    log.debug("FilteredPushTask.initialize() item title: " + itemTitle);
-                    navBox.add(btn);
+            NavBoxItemIFace btn = NavBox.createBtn(newSubscripTitle, newSubscripIcon, IconManager.STD_ICON_SIZE, defaultAction);
 
-                }
-                navBoxes.add(navBox);
-            }
+            subscripsNavBox.add(btn);
+
+            // TODO: FP MMK implement.  these should be draggable to trash
+
+            NavBoxItemIFace btn1 = NavBox.createBtn("Rubus", "Taxon", IconManager.STD_ICON_SIZE, defaultAction);
+            subscripsNavBox.add(btn1);
+
+            NavBoxItemIFace btn2 = NavBox.createBtn("Macklin", "Collector", IconManager.STD_ICON_SIZE, defaultAction);
+            subscripsNavBox.add(btn2);
+
+            NavBoxItemIFace btn3 = NavBox.createBtn("HUH", "Institution", IconManager.STD_ICON_SIZE, defaultAction);
+            subscripsNavBox.add(btn3);
+
+
+            navBoxes.add(subscripsNavBox);
+
         }
         
         // TODO: FP MMK just tucking this away for safekeeping
@@ -206,6 +209,8 @@ public class FilteredPushTask extends BaseTask
         extendedNavBoxes.clear();
         extendedNavBoxes.addAll(navBoxes);
 
+        // TODO: FP MMK if the query pane has not already been opened, clicking on one of these opens it
+        // in a non-accessible pane.  maybe recordset nav box items know how to fix that.
         QueryTask qTask = (QueryTask)ContextMgr.getTaskByClass(QueryTask.class);
         List<NavBoxIFace> qnbs = qTask.getNavBoxes();
         if (qnbs != null)
@@ -224,6 +229,96 @@ public class FilteredPushTask extends BaseTask
             extendedNavBoxes.addAll(rsnbs);
         }
         return extendedNavBoxes;
+    }
+    
+    /* (non-Javadoc)
+     * @see edu.ku.brc.specify.ui.CommandListener#doCommand(edu.ku.brc.specify.ui.CommandAction)
+     */
+    @Override
+    public void doCommand(CommandAction cmdAction)
+    {
+        super.doCommand(cmdAction);
+        
+        if (cmdAction.isType(FILTEREDPUSH))
+        {
+            processFpCommands(cmdAction);
+            
+        }
+    }
+    
+    private void processFpCommands(CommandAction cmdAction)
+    {
+        log.debug("processFpCommands");
+
+        if (cmdAction.isAction(NEW_SUBSCRIP) && cmdAction.getData() instanceof RecordSetIFace)
+        {
+            log.debug("adding to nav box");
+            addToNavBox((RecordSet) cmdAction.getData()); // TODO: FP MMK we should be adding a subscription, not a recordset.
+        }
+        else if (cmdAction.isAction(DEL_SUBSCRIP) & cmdAction.getData() instanceof RecordSetIFace)
+        {
+            log.debug("deleting from nav box");
+            deleteQueryFromUI(null, (RecordSet)cmdAction.getData());
+        }
+    }
+
+    /** FP MMK: took this from QueryTask
+     * 
+     * Adds a Query to the Left Pane NavBox (Refactor this with Workbench)
+     * @param query the Query to be added
+     * @return the nav box
+     */
+    protected NavBoxItemIFace addToNavBox(final RecordSet recordSet)
+    {
+        //boolean canDelete = AppContextMgr.isSecurityOn() ? getPermissions().canDelete() : true;
+        boolean canDelete = true;
+        final RolloverCommand roc = (RolloverCommand) makeDnDNavBtn(subscripsNavBox, recordSet.getName(),
+                "Query", null,
+                canDelete ? new CommandAction(FILTEREDPUSH, DEL_SUBSCRIP, recordSet) : null, 
+                true, false); // TODO: sort
+        //roc.setToolTip(getResourceString("QY_CLICK2EDIT"));
+        roc.setData(recordSet);
+        roc.addActionListener(new DisplayAction(subscripsBoxTitle));
+
+        NavBoxItemIFace nbi = (NavBoxItemIFace)roc;
+        
+        DBTableInfo tblInfo = DBTableIdMgr.getInstance().getInfoById(recordSet.getDbTableId()); // was getTableId; didn't work.
+        if (tblInfo != null)
+        {
+            ImageIcon rsIcon = tblInfo.getIcon(IconManager.STD_ICON_SIZE);
+            if (rsIcon != null)
+            {
+                nbi.setIcon(rsIcon);
+            }
+        }
+        
+        //roc.addDragDataFlavor(new DataFlavorTableExt(QueryTask.class, QUERY, recordSet.getTableId()));
+        if (canDelete)
+        {
+            roc.addDragDataFlavor(Trash.TRASH_FLAVOR);
+        }
+        
+        // FP MMK
+        //roc.addDragDataFlavor(FpPublish.FP_PUB_FLAVOR);
+
+        //boolean fpPubOK = true;
+        //CommandAction fpPubCmdAction = fpPubOK ? new CommandAction(QUERY, FP_PUB_RECORDSET, recordSet) : null;
+        //roc.setFpPublishCommandAction(fpPubCmdAction);
+        
+        return nbi;
+    }
+    
+    /** FP MMK: took from QueryTask
+     * 
+     * Delete the RecordSet from the UI, which really means remove the NavBoxItemIFace.
+     * This method first checks to see if the boxItem is not null and uses that, if
+     * it is null then it looks the box up by name and used that
+     * @param boxItem the box item to be deleted
+     * @param recordSets the record set that is "owned" by some UI object that needs to be deleted (used for secondary lookup
+     */
+    protected void deleteQueryFromUI(final NavBoxItemIFace boxItem, final RecordSet rs)
+    {
+        deleteDnDBtn(subscripsNavBox, boxItem != null ? boxItem : getBoxByTitle(subscripsNavBox, rs.getName()));
     }
     
     /**
