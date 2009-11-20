b5f7993d0d8d474e7f10723c8a15050ddbcdaa06
Maureen Kelly <mkelly>
2009-11-20 00:08:22 +0000

    using full name if it exists, otherwise author, otherwise collector for botanist loading into agent table

--- Specify/sql/botanist.sql
+++ Specify/sql/botanist.sql
@@ -2,7 +2,10 @@ select b.id,
        decode(b.team_flag, 1, 'true', '') as is_team,
        decode(b.corporate_flag, 1, 'true', '') as is_corporate,
        regexp_replace(nvl((select bn.name from botanist_name bn where bn.type_id=110101 and bn.botanist_id=b.id),
-                          (select bn.name from botanist_name bn where bn.type_id=110103 and bn.botanist_id=b.id)), '[[:space:]]+', ' ') as name,
+                           nvl((select bn.name from botanist_name bn where bn.type_id=110105 and bn.botanist_id=b.id),
+                               (select bn.name from botanist_name bn where bn.type_id=110103 and bn.botanist_id=b.id)
+                          )
+                      ), '[[:space:]]+', ' ') as name,
        (select name from st_lookup where id=b.dates_type_id) as dates_type,
        b.start_year,
        decode(b.start_precision_id, 113402, '?', 113403, 'circa', '') as start_precision,
