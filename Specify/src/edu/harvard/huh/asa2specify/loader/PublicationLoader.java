f70e6050d781ea99966630675920a617c97ad72e
Maureen Kelly <mkelly>
2009-12-19 00:10:04 +0000

    removed separate journal record creation; moved title to text1 and abbreviation to title; updated field length check for publisher and place of publication

--- Specify/src/edu/harvard/huh/asa2specify/loader/PublicationLoader.java
+++ Specify/src/edu/harvard/huh/asa2specify/loader/PublicationLoader.java
@@ -10,7 +10,6 @@ import edu.harvard.huh.asa2specify.LocalException;
 import edu.harvard.huh.asa2specify.SqlUtils;
 import edu.harvard.huh.asa2specify.lookup.PublicationLookup;
 import edu.ku.brc.specify.datamodel.Agent;
-import edu.ku.brc.specify.datamodel.Journal;
 import edu.ku.brc.specify.datamodel.ReferenceWork;
 
 public class PublicationLoader extends AuditedObjectLoader
@@ -30,21 +29,7 @@ public class PublicationLoader extends AuditedObjectLoader
 		Integer publicationId = publication.getId();
 		setCurrentRecordId(publicationId);
 		
-		Journal journal = null;
-
-		if (publication.isJournal())
-		{
-			journal = getJournal(publication);
-			String sql = getInsertSql(journal);
-			Integer journalId = insert(sql);
-			journal.setJournalId(journalId);
-		}
-		else
-		{
-			journal = new Journal();
-		}
-
-		ReferenceWork referenceWork = getReferenceWork(publication, journal);
+		ReferenceWork referenceWork = getReferenceWork(publication);
 
 		String sql = getInsertSql(referenceWork);
 		insert(sql);
@@ -107,7 +92,7 @@ public class PublicationLoader extends AuditedObjectLoader
 		return publication;
 	}
 
-	private ReferenceWork getReferenceWork(Publication publication, Journal journal) throws LocalException
+	private ReferenceWork getReferenceWork(Publication publication) throws LocalException
 	{		
 		ReferenceWork referenceWork = new ReferenceWork();
 
@@ -131,14 +116,11 @@ public class PublicationLoader extends AuditedObjectLoader
 			referenceWork.setIsbn(isbn);
 		}
 		
-		// Journal
-		referenceWork.setJournal(journal);
-		
 		// PlaceOfPublication
 		String pubPlace = publication.getPubPlace();
 		if (pubPlace != null)
 		{
-			pubPlace = truncate(pubPlace, 50, "place of publication");
+			pubPlace = truncate(pubPlace, 64, "place of publication");
 			referenceWork.setPlaceOfPublication(pubPlace);
 		}
 
@@ -146,7 +128,7 @@ public class PublicationLoader extends AuditedObjectLoader
 		String publisher = publication.getPublisher();
 		if (publisher != null)
 		{
-			publisher = truncate(publisher, 50, "publisher");
+			publisher = truncate(publisher, 64, "publisher");
 			referenceWork.setPublisher(publisher);
 		}
 
@@ -154,135 +136,62 @@ public class PublicationLoader extends AuditedObjectLoader
 		referenceWork.setReferenceWorkType(ReferenceWork.BOOK);
 		
 		// Remarks
-		String pubDate = publication.getPubDate();
 		String remarks = publication.getRemarks();
-		if (remarks == null) remarks = pubDate;
-		else if (pubDate != null) remarks = remarks + "[publication date: " + pubDate + "]";
-		
 		referenceWork.setRemarks(remarks);
 		
-		// Text1 (abbreviation)
-		String abbrev = publication.getAbbreviation();
-		if (journal == null) referenceWork.setText1(abbrev);
+		// Text1 (title)
+		String title = publication.getTitle();
+		referenceWork.setText1(title);
 
-		// Text2 (collation)
-		
+		// Text2 (publ date)
+		String pubDate = publication.getPubDate();
+		referenceWork.setWorkDate(pubDate);
+	      
 		// TimestampCreated
         Date dateCreated = publication.getDateCreated();
         referenceWork.setTimestampCreated(DateUtils.toTimestamp(dateCreated));
         
-		// Title
-		String title = publication.getTitle();
-		if (title == null)
-		{
-		    getLogger().warn(rec() + "Empty title");
-		    title = EMPTY;
-		    publication.setTitle(title);
-		}
-		title = truncate(title, 255, "title");
-		referenceWork.setTitle(title);
+		// Title (abbreviation)
+        String abbrev = publication.getAbbreviation();
+        if (abbrev == null)
+        {
+            getLogger().warn(rec() + "Empty abbreviation");
+            abbrev = EMPTY;
+            publication.setAbbreviation(abbrev);
+        }
+        referenceWork.setTitle(abbrev);
 
 		// URL
 		String url = publication.getUrl();
 		referenceWork.setUrl(url);
 
 		// WorkDate
-		if (pubDate != null)
-		{
-			if (pubDate.length() > 25) pubDate = pubDate.substring(0, 25);  // too many are much longer than this to call truncate
-			referenceWork.setWorkDate(pubDate);
-		}
 		
 		return referenceWork;
-	}
-
-	private Journal getJournal(Publication publication) throws LocalException
-	{
-		Integer publicationId = publication.getId();
-		checkNull(publicationId, "id");
-		
-		Journal journal = new Journal();
-		
-		// GUID: temporarily hold asa publication.id TODO: don't forget to unset this after migration
-		String guid = getGuid(publicationId);
-		journal.setGuid(guid);
-
-		// ISSN
-		String issn = publication.getIssn();
-		if (issn != null)
-		{
-			issn = truncate(issn, 16, "ISSN");
-			journal.setIssn(issn);
-		}
-
-		// JournalAbbreviation
-		String abbreviation = publication.getAbbreviation();
-		if (abbreviation != null)
-		{
-			abbreviation = truncate(abbreviation, 50, "abbreviation");
-			journal.setJournalAbbreviation(abbreviation);
-		}
-
-		// JournalName
-		String title = publication.getTitle();
-        if (title == null)
-        {
-            getLogger().warn(rec() + "Empty title");
-            title = EMPTY;
-            publication.setTitle(title);
-        }
-		title = truncate(title, 255, "title");
-		journal.setJournalName(title);
-
-		// Text1 (BPH)
-		String bph = publication.getBph();
-		journal.setText1(bph);
-		
-		// TimestampCreated
-		Date dateCreated = publication.getDateCreated();
-		journal.setTimestampCreated(DateUtils.toTimestamp(dateCreated));
-		
-		return journal;
 	}
 
     private String getGuid(Integer publicationId)
 	{
 		return publicationId + " publication";
 	}
-    
-	private String getInsertSql(Journal journal) throws LocalException
-	{
-		String fieldNames = "GUID, ISSN, JournalAbbreviation, JournalName, Text1, TimestampCreated, Version";
-
-		String[] values = new String[7];
-
-		values[0] = SqlUtils.sqlString( journal.getGuid());
-		values[1] = SqlUtils.sqlString( journal.getIssn());
-		values[2] = SqlUtils.sqlString( journal.getJournalAbbreviation());
-		values[3] = SqlUtils.sqlString( journal.getJournalName());
-		values[4] = SqlUtils.sqlString( journal.getText1());
-        values[5] = SqlUtils.sqlString( journal.getTimestampCreated());
-        values[6] = SqlUtils.zero();
-        
-		return SqlUtils.getInsertSql("journal", fieldNames, values);
-	}
 
 	private String getInsertSql(ReferenceWork referenceWork) throws LocalException
 	{
-		String fieldNames = "CreatedByAgentID, GUID, ISBN, JournalID, PlaceOfPublication, Publisher, " +
-				            "ReferenceWorkType, Remarks, Text1, TimestampCreated, Title, URL, Version, WorkDate";
+		String fieldNames = "CreatedByAgentID, GUID, ISBN, PlaceOfPublication, Publisher, " +
+				            "ReferenceWorkType, Remarks, Text1, Text2, TimestampCreated, " +
+				            "Title, URL, Version, WorkDate";
 
 		String[] values = new String[14];
 
 		values[0]  = SqlUtils.sqlString( referenceWork.getCreatedByAgent().getId());
 		values[1]  = SqlUtils.sqlString( referenceWork.getGuid());
 		values[2]  = SqlUtils.sqlString( referenceWork.getIsbn());
-		values[3]  = SqlUtils.sqlString( referenceWork.getJournal().getJournalId());
-		values[4]  = SqlUtils.sqlString( referenceWork.getPlaceOfPublication());
-		values[5]  = SqlUtils.sqlString( referenceWork.getPublisher());
-		values[6]  = SqlUtils.sqlString( referenceWork.getReferenceWorkType());
-		values[7]  = SqlUtils.sqlString( referenceWork.getRemarks());
-		values[8]  = SqlUtils.sqlString( referenceWork.getText1());
+		values[3]  = SqlUtils.sqlString( referenceWork.getPlaceOfPublication());
+		values[4]  = SqlUtils.sqlString( referenceWork.getPublisher());
+		values[5]  = SqlUtils.sqlString( referenceWork.getReferenceWorkType());
+		values[6]  = SqlUtils.sqlString( referenceWork.getRemarks());
+		values[7]  = SqlUtils.sqlString( referenceWork.getText1());
+		values[8]  = SqlUtils.sqlString( referenceWork.getText2());
         values[9]  = SqlUtils.sqlString( referenceWork.getTimestampCreated());
         values[10] = SqlUtils.sqlString( referenceWork.getTitle());
 		values[11] = SqlUtils.sqlString( referenceWork.getUrl());
