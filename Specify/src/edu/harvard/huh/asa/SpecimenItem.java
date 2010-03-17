948ef61292f136cc9f0891fdc18266c47476e0dc
Maureen Kelly <mkelly>
2010-03-17 23:52:16 +0000

    changed toString(CONTAINER_TYPE) to print out label rather than enum name

--- Specify/src/edu/harvard/huh/asa/SpecimenItem.java
+++ Specify/src/edu/harvard/huh/asa/SpecimenItem.java
@@ -11,13 +11,14 @@ public class SpecimenItem extends AuditedObject
 	public static enum FORMAT               { OnSheet,    InPacket,    InBox,    InBag,    InJar,    InSpiritMedium,     Wood,   Fossil,   OnMicroscopeSlide,     DNAsample,    Photograph,   Drawing,   ProtologOnSheet,     Other   };
 	public static String[] FormatNames =    { "on sheet", "in packet", "in box", "in bag", "in jar", "in spirit medium", "wood", "Fossil", "on microscope slide", "DNA sample", "photograph", "drawing", "protolog on sheet", "other" };
 
+    // this is for Specify data model PrepType
+    public static enum CONTAINER_TYPE       { Sheet,   Packet,   Box,  Bag,   Jar,   MicroscopeSlide,     Slide35mm,     Self,   Other,   Logical   };
+    public static String[] ContainerNames = { "Sheet", "Packet", "Box","Bag", "Jar", "Microscope slide",  "35 mm slide", "Self", "Other", "Logical" };
+
 	// this is for data model Fragment PrepMethod
 	public static enum PREP_METHOD            { Pressed,    Dried,   Wood,   Drawing,   SpiritMedium,    Protolog,   Photograph,   Glycerine,   Fossil,   DNAsample,    Other   };
 	public static String[] PrepMethodNames =  { "Pressed",  "Dried", "Wood", "Drawing", "Spirit medium", "Protolog", "Photograph", "Glycerine", "Fossil", "DNA sample", "Other" };
 
-	// this is for Specify data model PrepType
-	public static enum CONTAINER_TYPE       { Sheet,   Packet,   Bag,   Box,   MicroscopeSlide,    Jar,   Slide35mm,     Self,   Other,   Logical   };
-	public static String[] ContainerNames = { "Sheet", "Packet", "Bag", "Box", "Microscope slide", "Jar", "35 mm slide", "Self", "Other", "Logical" };
 
 	public static REPRO_STATUS parseReproStatus(String string) throws AsaException
 	{
@@ -48,7 +49,7 @@ public class SpecimenItem extends AuditedObject
 	
 	public static String toString(FORMAT format)
     {
-        return format.name();
+        return FormatNames[format.ordinal()];
     }
 	
 	public static String toString(CONTAINER_TYPE containerType)
