ce177db3c0727a84b416ce7184a972891c807804
Maureen Kelly <mkelly>
2010-03-19 22:58:30 +0000

    overriding beforeMerge to fill in collection for collectionObject; overriding afterFillForm to create new Preparation object

--- Specify/src/edu/harvard/huh/specify/datamodel/busrules/HUHFragmentBusRules.java
+++ Specify/src/edu/harvard/huh/specify/datamodel/busrules/HUHFragmentBusRules.java
@@ -1,68 +1,84 @@
 package edu.harvard.huh.specify.datamodel.busrules;
 
-import static edu.ku.brc.ui.UIRegistry.getResourceString;
-
 import java.util.List;
 
+import edu.ku.brc.af.core.AppContextMgr;
+import edu.ku.brc.af.ui.forms.BaseBusRules;
 import edu.ku.brc.af.ui.forms.BusinessRulesIFace;
 import edu.ku.brc.af.ui.forms.BusinessRulesOkDeleteIFace;
 import edu.ku.brc.af.ui.forms.DraggableRecordIdentifier;
 import edu.ku.brc.af.ui.forms.Viewable;
 import edu.ku.brc.dbsupport.DataProviderSessionIFace;
-import edu.ku.brc.specify.datamodel.Determination;
+import edu.ku.brc.specify.datamodel.Agent;
+import edu.ku.brc.specify.datamodel.Collection;
+import edu.ku.brc.specify.datamodel.CollectionObject;
+import edu.ku.brc.specify.datamodel.DataModelObjBase;
 import edu.ku.brc.specify.datamodel.Fragment;
-import edu.ku.brc.af.ui.forms.BaseBusRules;
+import edu.ku.brc.specify.datamodel.Preparation;
+import edu.ku.brc.specify.datamodel.busrules.CollectionObjectBusRules;
 
-public class HUHFragmentBusRules extends BaseBusRules implements
-		BusinessRulesIFace {
+public class HUHFragmentBusRules extends BaseBusRules implements BusinessRulesIFace
+{
 
 	@Override
-	public void aboutToShutdown() {
+	public void aboutToShutdown()
+	{
 		// TODO Auto-generated method stub
 		super.aboutToShutdown();
 	}
 
 	@Override
-	public void addChildrenToNewDataObjects(Object newDataObj) {
-		// TODO Auto-generated method stub
-		super.addChildrenToNewDataObjects(newDataObj);
+	public void addChildrenToNewDataObjects(Object newDataObj)
+	{
+	    // TODO Auto-generated method stub
+	    super.addChildrenToNewDataObjects(newDataObj);
 	}
 
 	@Override
-	public void afterDeleteCommit(Object dataObj) {
+	public void afterDeleteCommit(Object dataObj)
+	{
 		// TODO Auto-generated method stub
 		super.afterDeleteCommit(dataObj);
 	}
 
 	@Override
-	public void afterFillForm(Object dataObj) {
-		// TODO Auto-generated method stub
-		super.afterFillForm(dataObj);
+	public void afterFillForm(Object dataObj)
+	{
+		Fragment fragment = (Fragment) dataObj;
+		
+		if (fragment != null && fragment.getPreparation() == null)
+		{
+		    Preparation prep = new Preparation();
+		    prep.initialize();
+		    fragment.addReference(prep, "preparation");
+		}
 	}
 
 	@Override
-	public boolean afterSaveCommit(Object dataObj,
-			DataProviderSessionIFace session) {
+	public boolean afterSaveCommit(Object dataObj, DataProviderSessionIFace session)
+	{
 		// TODO Auto-generated method stub
 		return super.afterSaveCommit(dataObj, session);
 	}
 
 	@Override
-	public void afterSaveFailure(Object dataObj,
-			DataProviderSessionIFace session) {
+	public void afterSaveFailure(Object dataObj, DataProviderSessionIFace session)
+	{
 		// TODO Auto-generated method stub
 		super.afterSaveFailure(dataObj, session);
 	}
 
 	@Override
-	public void beforeDelete(Object dataObj, DataProviderSessionIFace session) {
+	public void beforeDelete(Object dataObj, DataProviderSessionIFace session)
+	{
 		// TODO Auto-generated method stub
 		super.beforeDelete(dataObj, session);
 	}
 
 	@Override
-	public boolean beforeDeleteCommit(Object dataObj,
-			DataProviderSessionIFace session) throws Exception {
+	public boolean beforeDeleteCommit(Object dataObj, DataProviderSessionIFace session)
+	    throws Exception
+	{
 		// TODO Auto-generated method stub
 		return super.beforeDeleteCommit(dataObj, session);
 	}
@@ -74,134 +90,197 @@ public class HUHFragmentBusRules extends BaseBusRules implements
 	}
 
 	@Override
-	public void beforeMerge(Object dataObj, DataProviderSessionIFace session) {
-		// TODO Auto-generated method stub
-		super.beforeMerge(dataObj, session);
+	public void beforeMerge(Object dataObj, DataProviderSessionIFace session)
+	{
+	    // move this to a method called after form is filled so that required cataloger
+	    // field doesn't have to be entered by user
+		Fragment fragment = (Fragment) dataObj;
+		
+		if (fragment != null)
+		{
+		    CollectionObject collObj = fragment.getCollectionObject();
+
+		    if (collObj != null)
+		    {
+		        if (collObj.getCollection() == null)
+		        {
+		            Collection catSeries = AppContextMgr.getInstance().getClassObject(Collection.class);
+		            collObj.setCollection(catSeries); 
+		        }
+
+		        Agent agent = Agent.getUserAgent();
+		        if (agent != null)
+		        {
+		            collObj.setCataloger(agent);
+		        }
+		        saveObject(collObj, session);
+		    }
+
+		}
 	}
 
 	@Override
-	public void beforeSave(Object dataObj, DataProviderSessionIFace session) {
+	public void beforeSave(Object dataObj, DataProviderSessionIFace session)
+	{
 		// TODO Auto-generated method stub
 		super.beforeSave(dataObj, session);
 	}
 
 	@Override
-	public boolean beforeSaveCommit(Object dataObj,
-			DataProviderSessionIFace session) throws Exception {
+	public boolean beforeSaveCommit(Object dataObj, DataProviderSessionIFace session)
+	    throws Exception
+	{
 		// TODO Auto-generated method stub
 		return super.beforeSaveCommit(dataObj, session);
 	}
 
 	@Override
-	public boolean canCreateNewDataObject() {
+	public boolean canCreateNewDataObject()
+	{
 		// TODO Auto-generated method stub
 		return super.canCreateNewDataObject();
 	}
 
 	@Override
-	public void createNewObj(boolean doSetIntoAndValidateArg, Object oldDataObj) {
+	public void createNewObj(boolean doSetIntoAndValidateArg, Object oldDataObj)
+	{
 		// TODO Auto-generated method stub
 		super.createNewObj(doSetIntoAndValidateArg, oldDataObj);
 	}
 
 	@Override
-	public boolean doesSearchObjectRequireNewParent() {
+	public boolean doesSearchObjectRequireNewParent()
+	{
 		// TODO Auto-generated method stub
 		return super.doesSearchObjectRequireNewParent();
 	}
 
 	@Override
-	public void formShutdown() {
+	public void formShutdown()
+	{
 		// TODO Auto-generated method stub
 		super.formShutdown();
 	}
 
 	@Override
-	public String getDeleteMsg(Object dataObj) {
+	public String getDeleteMsg(Object dataObj)
+	{
 		// TODO Auto-generated method stub
 		return super.getDeleteMsg(dataObj);
 	}
 
 	@Override
-	public String getMessagesAsString() {
+	public String getMessagesAsString()
+	{
 		// TODO Auto-generated method stub
 		return super.getMessagesAsString();
 	}
 
 	@Override
-	public List<String> getWarningsAndErrors() {
+	public List<String> getWarningsAndErrors()
+	{
 		// TODO Auto-generated method stub
 		return super.getWarningsAndErrors();
 	}
 
 	@Override
-	public void initialize(Viewable viewable) {
+	public void initialize(Viewable viewable)
+	{
 		// TODO Auto-generated method stub
 		super.initialize(viewable);
 	}
 
 	@Override
-	public boolean isOkToAssociateSearchObject(Object newParentDataObj,
-			Object dataObjectFromSearch) {
+	public boolean isOkToAssociateSearchObject(Object newParentDataObj, Object dataObjectFromSearch)
+	{
 		// TODO Auto-generated method stub
 		return super.isOkToAssociateSearchObject(newParentDataObj, dataObjectFromSearch);
 	}
 
 	@Override
-	public boolean isOkToSave(Object dataObj, DataProviderSessionIFace session) {
+	public boolean isOkToSave(Object dataObj, DataProviderSessionIFace session)
+	{
 		// TODO Auto-generated method stub
 		return super.isOkToSave(dataObj, session);
 	}
 
 	@Override
-	public void okToDelete(Object dataObj, DataProviderSessionIFace session,
-			BusinessRulesOkDeleteIFace deletable) {
+	public void okToDelete(Object dataObj, DataProviderSessionIFace session, BusinessRulesOkDeleteIFace deletable)
+	{
 		// TODO Auto-generated method stub
 		super.okToDelete(dataObj, session, deletable);
 	}
 
 	@Override
-	public boolean okToEnableDelete(Object dataObj) {
+	public boolean okToEnableDelete(Object dataObj)
+	{
 		// TODO Auto-generated method stub
 		return super.okToEnableDelete(dataObj);
 	}
 
 	@Override
-	public STATUS processBusinessRules(Object dataObj) {
+	public STATUS processBusinessRules(Object dataObj)
+	{
 		// TODO Auto-generated method stub
 		return super.processBusinessRules(dataObj);
 	}
 
 	@Override
-	public STATUS processBusinessRules(Object parentDataObj, Object dataObj,
-			boolean isExistingObject) {
+	public STATUS processBusinessRules(Object parentDataObj, Object dataObj, boolean isExistingObject)
+	{
 		// TODO Auto-generated method stub
 		return super.processBusinessRules(parentDataObj, dataObj, isExistingObject);
 	}
 
 	@Override
-	public Object processSearchObject(Object newParentDataObj,
-			Object dataObjectFromSearch) {
+	public Object processSearchObject(Object newParentDataObj, Object dataObjectFromSearch)
+	{
 		// TODO Auto-generated method stub
 		return super.processSearchObject(newParentDataObj, dataObjectFromSearch);
 	}
 
 	@Override
-	public void setObjectIdentity(Object dataObj,
-			DraggableRecordIdentifier draggableIcon) {
+	public void setObjectIdentity(Object dataObj, DraggableRecordIdentifier draggableIcon)
+	{
 		// TODO Auto-generated method stub
 		super.setObjectIdentity(dataObj, draggableIcon);
 	}
 
 	@Override
-	public boolean shouldCloneField(String fieldName) {
+	public boolean shouldCloneField(String fieldName)
+	{
 		// TODO Auto-generated method stub
 		return super.shouldCloneField(fieldName);
 	}
 
 	@Override
-	public boolean shouldCreateSubViewData(String fieldName) {
+	public boolean shouldCreateSubViewData(String fieldName)
+	{
 		// TODO Auto-generated method stub
 		return super.shouldCreateSubViewData(fieldName);
 	}
+    
+    private void saveObject(final DataModelObjBase obj, final DataProviderSessionIFace session)
+    {
+        if (obj != null)
+        {
+            try
+            {
+                if (obj.getId() != null)
+                {
+                    session.merge(obj);
+                }
+                else
+                {
+                    session.save(obj);
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.printStackTrace();
+                edu.ku.brc.af.core.UsageTracker.incrHandledUsageCount();
+                edu.ku.brc.exceptions.ExceptionTracker.getInstance().capture(CollectionObjectBusRules.class, ex);
+            }
+        }
+    }
 }
