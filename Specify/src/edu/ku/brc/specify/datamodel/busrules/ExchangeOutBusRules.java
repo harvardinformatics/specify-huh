4c283834e88d851c597415dda23a63b78bb1db9b
Maureen Kelly <mkelly>
2010-03-30 22:28:17 +0000

    added code to make ExchangeOutBusRules act similarly to GiftBusRules

--- Specify/src/edu/ku/brc/specify/datamodel/busrules/ExchangeOutBusRules.java
+++ Specify/src/edu/ku/brc/specify/datamodel/busrules/ExchangeOutBusRules.java
@@ -19,13 +19,21 @@
 */
 package edu.ku.brc.specify.datamodel.busrules;
 
+import static edu.ku.brc.ui.UIRegistry.getLocalizedMessage;
 import edu.ku.brc.af.core.AppContextMgr;
 import edu.ku.brc.af.ui.forms.BaseBusRules;
+import edu.ku.brc.af.ui.forms.DraggableRecordIdentifier;
+import edu.ku.brc.af.ui.forms.FormDataObjIFace;
+import edu.ku.brc.dbsupport.RecordSetIFace;
+import edu.ku.brc.specify.datamodel.Accession;
 import edu.ku.brc.specify.datamodel.Division;
 import edu.ku.brc.specify.datamodel.ExchangeOut;
+import edu.ku.brc.specify.datamodel.RecordSet;
+import edu.ku.brc.ui.CommandAction;
+import edu.ku.brc.ui.CommandDispatcher;
 
 /**
- * @author rod
+ * @author rod; revised by mmk
  *
  * @code_status Alpha
  *
@@ -34,6 +42,8 @@ import edu.ku.brc.specify.datamodel.ExchangeOut;
  */
 public class ExchangeOutBusRules extends BaseBusRules
 {
+    public static final String CMDTYPE   = "Interactions";
+    public final String NEW_EXCHANGE_OUT = "NEW_EXCHANGE_OUT";
 
     /**
      * @param dataClasses
@@ -52,4 +62,81 @@ public class ExchangeOutBusRules extends BaseBusRules
         ((ExchangeOut)newDataObj).setDivision(AppContextMgr.getInstance().getClassObject(Division.class));
     }
 
+    /* (non-Javadoc)
+     * @see edu.ku.brc.ui.forms.BaseBusRules#processBusinessRules(java.lang.Object)
+     */
+    @Override
+    public STATUS processBusinessRules(Object dataObj)
+    {
+        reasonList.clear();
+        
+        if (!(dataObj instanceof ExchangeOut))
+        {
+            return STATUS.Error;
+        }
+        
+        STATUS duplicateNumberStatus = isCheckDuplicateNumberOK("exchangeNumber", 
+                                                                (FormDataObjIFace)dataObj, 
+                                                                ExchangeOut.class, 
+                                                                "exchangeOutId");
+
+        return duplicateNumberStatus;
+    }
+    
+    /* (non-Javadoc)
+     * @see edu.ku.brc.ui.forms.BusinessRulesIFace#deleteMsg(java.lang.Object)
+     */
+    public String getDeleteMsg(final Object dataObj)
+    {
+        if (dataObj instanceof Accession)
+        {
+            return getLocalizedMessage("LOAN_DELETED", ((ExchangeOut)dataObj).getExchangeNumber());
+        }
+        // else
+        return super.getDeleteMsg(dataObj);
+    }
+    
+    /* (non-Javadoc)
+     * @see edu.ku.brc.ui.forms.BusinessRulesIFace#setObjectIdentity(java.lang.Object, edu.ku.brc.ui.DraggableIcon)
+     */
+    public void setObjectIdentity(final Object dataObj, 
+                                  final DraggableRecordIdentifier draggableIcon)
+    {
+        if (dataObj == null)
+        {
+            draggableIcon.setLabel("");
+        }
+        
+        if (dataObj instanceof ExchangeOut)
+        {
+            ExchangeOut exchangeOut = (ExchangeOut)dataObj;
+            
+            draggableIcon.setLabel(exchangeOut.getExchangeNumber());
+            
+            Object data = draggableIcon.getData();
+            if (data == null)
+            {
+                RecordSet rs = new RecordSet();
+                rs.initialize();
+                rs.addItem(exchangeOut.getExchangeOutId());
+                data = rs;
+                draggableIcon.setData(data);
+                
+            } else if (data instanceof RecordSetIFace)
+            {
+                RecordSetIFace rs = (RecordSetIFace)data;
+                rs.clearItems();
+                rs.addItem(exchangeOut.getExchangeOutId());
+            }
+        }
+     }
+
+    /* (non-Javadoc)
+     * @see edu.ku.brc.af.ui.forms.BaseBusRules#canCreateNewDataObject()
+     */
+    @Override
+    public boolean canCreateNewDataObject()
+    {
+        return true;
+    }
 }
