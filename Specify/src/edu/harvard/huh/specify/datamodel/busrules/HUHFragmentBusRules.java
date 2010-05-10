b00730bcb886d4548c737d9dda952b693c0e5027
Maureen Kelly <mkelly>
2010-05-10 20:36:17 +0000

    major overhaul, including removal of stub methods, expansion of addChildrenToNewDataObject

--- Specify/src/edu/harvard/huh/specify/datamodel/busrules/HUHFragmentBusRules.java
+++ Specify/src/edu/harvard/huh/specify/datamodel/busrules/HUHFragmentBusRules.java
@@ -1,218 +1,84 @@
 package edu.harvard.huh.specify.datamodel.busrules;
 
+import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
-import edu.ku.brc.af.core.AppContextMgr;
 import edu.ku.brc.af.core.db.DBFieldInfo;
 import edu.ku.brc.af.core.db.DBTableIdMgr;
 import edu.ku.brc.af.core.db.DBTableInfo;
 import edu.ku.brc.af.core.expresssearch.QueryAdjusterForDomain;
 import edu.ku.brc.af.ui.forms.BaseBusRules;
 import edu.ku.brc.af.ui.forms.BusinessRulesIFace;
-import edu.ku.brc.af.ui.forms.BusinessRulesOkDeleteIFace;
-import edu.ku.brc.af.ui.forms.DraggableRecordIdentifier;
 import edu.ku.brc.af.ui.forms.FormDataObjIFace;
 import edu.ku.brc.af.ui.forms.FormHelper;
-import edu.ku.brc.af.ui.forms.Viewable;
+import edu.ku.brc.af.ui.forms.formatters.UIFieldFormatterIFace;
 import edu.ku.brc.dbsupport.DataProviderSessionIFace;
 import edu.ku.brc.specify.conversion.BasicSQLUtils;
-import edu.ku.brc.specify.datamodel.Collection;
+import edu.ku.brc.specify.datamodel.Agent;
+import edu.ku.brc.specify.datamodel.CollectingEvent;
 import edu.ku.brc.specify.datamodel.CollectionObject;
 import edu.ku.brc.specify.datamodel.DataModelObjBase;
 import edu.ku.brc.specify.datamodel.Fragment;
+import edu.ku.brc.specify.datamodel.Locality;
 import edu.ku.brc.specify.datamodel.Preparation;
-import edu.ku.brc.specify.datamodel.busrules.CollectionObjectBusRules;
 
 public class HUHFragmentBusRules extends BaseBusRules implements BusinessRulesIFace
 {
     private static final Logger  log   = Logger.getLogger(HUHFragmentBusRules.class);
+
+    public HUHFragmentBusRules()
+    {
+        super();
+    }
+
+    @Override
+    public void addChildrenToNewDataObjects(Object newDataObj)
+    {
+        Fragment fragment = (Fragment) newDataObj;
+        
+        CollectionObject collectionObject = fragment.getCollectionObject();
+
+        if (collectionObject == null)
+        {
+            collectionObject = new CollectionObject();
+            collectionObject.initialize();
+            fragment.setCollectionObject(collectionObject);
+        }
+        
+        if (collectionObject.getCataloger() == null)
+        {
+            Agent agent = Agent.getUserAgent();
+            collectionObject.setCataloger(agent);
+            collectionObject.setCatalogedDate(Calendar.getInstance());
+            collectionObject.setCatalogedDatePrecision((byte) UIFieldFormatterIFace.PartialDateEnum.Full.ordinal());
+        }
+
+        CollectingEvent collectingEvent = collectionObject.getCollectingEvent();
+        if (collectingEvent == null)
+        {
+            collectingEvent = new CollectingEvent();
+            collectingEvent.initialize();
+            collectionObject.setCollectingEvent(collectingEvent);
+        }
+
+        Locality locality = collectingEvent.getLocality();
+        if (locality == null)
+        {
+            locality = new Locality();
+            locality.initialize();
+            collectingEvent.setLocality(locality);
+        }
+    }
     
-	@Override
-	public void aboutToShutdown()
-	{
-		// TODO Auto-generated method stub
-		super.aboutToShutdown();
-	}
-
-	@Override
-	public void addChildrenToNewDataObjects(Object newDataObj)
-	{
-	    super.addChildrenToNewDataObjects(newDataObj);
-	       Fragment fragment = (Fragment) newDataObj;
-	        
-	        if (fragment != null)
-	        {
-	            if (fragment.getPreparation() == null)
-	            {
-	                Preparation prep = new Preparation();
-	                prep.initialize();
-	                fragment.addReference(prep, "preparation");
-	            }
-	        }
-	}
-
-	@Override
-	public void afterDeleteCommit(Object dataObj)
-	{
-		// TODO Auto-generated method stub
-		super.afterDeleteCommit(dataObj);
-	}
-
 	@Override
 	public void afterFillForm(Object dataObj)
 	{
 
 	}
-
-	@Override
-	public boolean afterSaveCommit(Object dataObj, DataProviderSessionIFace session)
-	{
-		// TODO Auto-generated method stub
-		return super.afterSaveCommit(dataObj, session);
-	}
-
-	@Override
-	public void afterSaveFailure(Object dataObj, DataProviderSessionIFace session)
-	{
-		// TODO Auto-generated method stub
-		super.afterSaveFailure(dataObj, session);
-	}
-
-	@Override
-	public void beforeDelete(Object dataObj, DataProviderSessionIFace session)
-	{
-		// TODO Auto-generated method stub
-		super.beforeDelete(dataObj, session);
-	}
-
-	@Override
-	public boolean beforeDeleteCommit(Object dataObj, DataProviderSessionIFace session)
-	    throws Exception
-	{
-		// TODO Auto-generated method stub
-		return super.beforeDeleteCommit(dataObj, session);
-	}
-
-	@Override
-	public void beforeFormFill() {
-		// TODO Auto-generated method stub
-		super.beforeFormFill();
-	}
-
-	@Override
-	public void beforeMerge(Object dataObj, DataProviderSessionIFace session)
-	{
-
-		Fragment fragment = (Fragment) dataObj;
-		
-		if (fragment != null)
-		{
-		    CollectionObject collObj = fragment.getCollectionObject();
-		    boolean newColObj = false;
-		    
-		    if (collObj == null)
-		    {
-		        collObj = new CollectionObject();
-		        collObj.initialize();
-		        newColObj = true;
-		    }
-            if (collObj.getCollection() == null)
-            {
-                Collection catSeries = AppContextMgr.getInstance().getClassObject(Collection.class);
-                collObj.setCollection(catSeries); 
-            }
-            saveObject(collObj, session);
-            if (newColObj) fragment.addReference(collObj, "collectionObject");
-            
-            Preparation prep = fragment.getPreparation();
-            boolean newPrep = false;
-            
-            if (prep == null)
-            {
-                prep = new Preparation();
-                prep.initialize();
-                newPrep = true;
-            }
-            saveObject(prep, session);
-            if (newPrep) fragment.addReference(prep, "preparation");
-		}
-	}
-
-	@Override
-	public void beforeSave(Object dataObj, DataProviderSessionIFace session)
-	{
-		// TODO Auto-generated method stub
-		super.beforeSave(dataObj, session);
-	}
-
-	@Override
-	public boolean beforeSaveCommit(Object dataObj, DataProviderSessionIFace session)
-	    throws Exception
-	{
-		// TODO Auto-generated method stub
-		return super.beforeSaveCommit(dataObj, session);
-	}
-
-	@Override
-	public boolean canCreateNewDataObject()
-	{
-		// TODO Auto-generated method stub
-		return super.canCreateNewDataObject();
-	}
-
-	@Override
-	public void createNewObj(boolean doSetIntoAndValidateArg, Object oldDataObj)
-	{
-		// TODO Auto-generated method stub
-		super.createNewObj(doSetIntoAndValidateArg, oldDataObj);
-	}
-
-	@Override
-	public boolean doesSearchObjectRequireNewParent()
-	{
-		// TODO Auto-generated method stub
-		return super.doesSearchObjectRequireNewParent();
-	}
-
-	@Override
-	public void formShutdown()
-	{
-		// TODO Auto-generated method stub
-		super.formShutdown();
-	}
-
-	@Override
-	public String getDeleteMsg(Object dataObj)
-	{
-		// TODO Auto-generated method stub
-		return super.getDeleteMsg(dataObj);
-	}
-
-	@Override
-	public String getMessagesAsString()
-	{
-		// TODO Auto-generated method stub
-		return super.getMessagesAsString();
-	}
-
-	@Override
-	public List<String> getWarningsAndErrors()
-	{
-		// TODO Auto-generated method stub
-		return super.getWarningsAndErrors();
-	}
-
-	@Override
-	public void initialize(Viewable viewable)
-	{
-		// TODO Auto-generated method stub
-		super.initialize(viewable);
-	}
-
+    
     /** Either the Fragment or its Preparation must have a barcode, and the barcode
      *  must be unique across the union of all Fragment and Preparation objects .
      * @param dataObj the Fragment to check
@@ -220,90 +86,72 @@ public class HUHFragmentBusRules extends BaseBusRules implements BusinessRulesIF
      */
     protected STATUS isBarcodeOK(final FormDataObjIFace dataObj)
     {
-        String fieldName = "identifier";
-        
-        Class<?> fragmentClass = Fragment.class;
-        Fragment fragment = (Fragment) dataObj;
-        
-        DBTableInfo fragmentTableInfo = DBTableIdMgr.getInstance().getByClassName(fragmentClass.getName());
-        DBFieldInfo fragmentFieldInfo = fragmentTableInfo.getFieldByName(fieldName);
-        
-        String fragmentBarcode = (String)FormHelper.getValue(fragment, fieldName);
-        String formattedFragmentBarcode = "";
-        if (fragmentFieldInfo != null && fragmentFieldInfo.getFormatter() != null && fragmentBarcode != null)
+        if (dataObj instanceof Fragment)
         {
-            Object fmtObj = fragmentFieldInfo.getFormatter().formatToUI(fragmentBarcode);
-            if (fmtObj != null)
+            Fragment fragment = (Fragment) dataObj;
+            
+            String fieldName = "identifier";
+            String barcode = (String)FormHelper.getValue(fragment, fieldName);
+            String formattedBarcode = formatToUI(Fragment.class, fieldName, barcode);
+            
+            Integer prepCount = 0;
+            Integer fragmentCount = 0;
+            if (!StringUtils.isEmpty(barcode))
             {
-                formattedFragmentBarcode = fmtObj.toString();
+                // count preparations with this barcode
+                prepCount =
+                    HUHFragmentBusRules.getCountSql(Preparation.class, "preparationId", fieldName, barcode, null);
+                
+                // count fragment records with this barcode
+                fragmentCount =
+                    HUHFragmentBusRules.getCountSql(Fragment.class, "fragmentId", fieldName, barcode, fragment.getId());
             }
-        }
-        
-        Class<?> prepClass = Preparation.class;
-        Preparation prep  = fragment.getPreparation();
-        
-        DBTableInfo prepTableInfo = DBTableIdMgr.getInstance().getByClassName(prepClass.getName());
-        DBFieldInfo prepFieldInfo = prepTableInfo.getFieldByName(fieldName);
-
-        String prepBarcode = (String)FormHelper.getValue(prep, fieldName);
-        String formattedPrepBarcode = "";
-        if (prepFieldInfo != null && prepFieldInfo.getFormatter() != null && prepBarcode != null)
-        {
-            Object fmtObj = prepFieldInfo.getFormatter().formatToUI(prepBarcode);
-            if (fmtObj != null)
+            
+            if (fragmentCount + prepCount == 0)
             {
-                formattedPrepBarcode = fmtObj.toString();
+                if (!StringUtils.isEmpty(barcode))
+                {
+                    // fragment has the barcode
+                    return STATUS.OK;
+                }
+                else
+                {
+                    if (fragment.getPreparation() != null &&
+                            fragment.getPreparation().getIdentifier() != null)
+                    {
+                        // preparation has the barcode
+                        return STATUS.OK;
+                    }
+                    reasonList.add(getErrorMsg("GENERIC_FIELD_MISSING", Fragment.class, fieldName, ""));
+                    return STATUS.Error;
+                }
             }
-        }
 
-        if (StringUtils.isEmpty(fragmentBarcode) && StringUtils.isEmpty(prepBarcode))
-        {
-            reasonList.add(getErrorMsg("GENERIC_FIELD_MISSING", fragmentClass, fieldName, formattedFragmentBarcode));
-
+            reasonList.add(getErrorMsg("GENERIC_FIELD_IN_USE", Fragment.class, fieldName, formattedBarcode));
             return STATUS.Error;
         }
         
-        if (!StringUtils.isEmpty(fragmentBarcode) && fragmentBarcode.equals(prepBarcode))
-        {
-            reasonList.add(getErrorMsg("GENERIC_FIELD_IN_USE", fragmentClass, fieldName, formattedFragmentBarcode));
-
-            return STATUS.Error;
-        }
-
-        // Let's check for duplicates of the fragment identifier
-        Integer fragmentCount = getCountSql(fragmentClass, "fragmentId", fieldName, fragmentBarcode, dataObj.getId());
-        if (fragmentCount == null) fragmentCount = 0;
-
-        Integer prepCount = getCountSql(Preparation.class, "preparationId", fieldName, fragmentBarcode, null);
-        if (prepCount == null) prepCount = 0;
-
-        if (fragmentCount + prepCount != 0)
-        {
-            reasonList.add(getErrorMsg("GENERIC_FIELD_IN_USE", fragmentClass, fieldName, formattedFragmentBarcode));
-
-            return STATUS.Error;
-        }
-        
-        // Check for duplicates of the preparation identifier
-        fragmentCount = getCountSql(fragmentClass, "fragmentId", fieldName, prepBarcode, null);
-        if (fragmentCount == null) fragmentCount = 0;
-        
-        prepCount = getCountSql(Preparation.class, "preparationId", fieldName, fragmentBarcode, prep.getId());
-        if (prepCount == null) prepCount = 0;
-        
-        if (fragmentCount + prepCount == 0)
-        {
-            reasonList.add(getErrorMsg("GENERIC_FIELD_IN_USE", fragmentClass, fieldName, formattedPrepBarcode));
-        }
-        
-        return STATUS.OK;
+        throw new IllegalArgumentException();
     }
     
-    private int getCountSql(final Class<?> dataClass, final String primaryFieldName, final String fieldName, final String fieldValue, final Integer id)
+    /**
+     * Search the table represented by dataClass for fields with fieldName having value fieldValue; omit records with primaryFieldName
+     * having value id.
+     * 
+     * @param dataClass
+     * @param primaryFieldName
+     * @param fieldName
+     * @param fieldValue
+     * @param id
+     * @return
+     */
+    static int getCountSql(final Class<?> dataClass, final String primaryFieldName, final String fieldName, final String fieldValue, final Integer id)
     {
         DBTableInfo tableInfo = DBTableIdMgr.getInstance().getByClassName(dataClass.getName());
+        DBFieldInfo fieldInfo = tableInfo.getFieldByName(fieldName);
         DBFieldInfo primaryFieldInfo = tableInfo.getFieldByName(primaryFieldName);
         
+        // find a primary key column to do count() on
         String  countColumnName = null;
         if (primaryFieldInfo != null)
         {
@@ -316,101 +164,49 @@ public class HUHFragmentBusRules extends BaseBusRules implements BusinessRulesIF
             }
         }
         
-        DBFieldInfo fieldInfo = tableInfo.getFieldByName(fieldName);
+        // do we have to wrap the field value in quotes?
         String quote = fieldInfo.getDataClass() == String.class || fieldInfo.getDataClass() == Date.class ? "'" : "";
         
+        // compose sql: select count(primary_key) from table_name where field_name = field_value
         String tableName = tableInfo.getName();
         String compareColumName = fieldInfo.getColumn();
         String sql = String.format("SELECT COUNT(%s) FROM %s WHERE %s = %s", countColumnName, tableName, compareColumName, quote + fieldValue + quote);
 
+        // if we should ignore records with a given primary_key from the result, add a clause: and primary_key <> id_value
         if (id != null)
         {
             sql += " AND " + countColumnName + " <> " + id;
         }
 
+        // not sure what special columns might be but I saw this called somewhere else
         String special = QueryAdjusterForDomain.getInstance().getSpecialColumns(tableInfo, false);
         sql += StringUtils.isNotEmpty(special) ? (" AND "+special) : "";
         
         log.debug(sql);
         
-        return BasicSQLUtils.getCount(sql);
+        Integer count = BasicSQLUtils.getCount(sql);
+        return count == null ? 0 : count;
     }
 
-    @Override
-	public boolean isOkToAssociateSearchObject(Object newParentDataObj, Object dataObjectFromSearch)
-	{
-		// TODO Auto-generated method stub
-		return super.isOkToAssociateSearchObject(newParentDataObj, dataObjectFromSearch);
-	}
-
-	@Override
-	public boolean isOkToSave(Object dataObj, DataProviderSessionIFace session)
-	{
-		// TODO Auto-generated method stub
-		return super.isOkToSave(dataObj, session);
-	}
-
-	@Override
-	public void okToDelete(Object dataObj, DataProviderSessionIFace session, BusinessRulesOkDeleteIFace deletable)
-	{
-		// TODO Auto-generated method stub
-		super.okToDelete(dataObj, session, deletable);
-	}
-
-	@Override
-	public boolean okToEnableDelete(Object dataObj)
-	{
-		// TODO Auto-generated method stub
-		return super.okToEnableDelete(dataObj);
-	}
-
-	@Override
-	public STATUS processBusinessRules(Object dataObj)
-	{
-		STATUS status = super.processBusinessRules(dataObj);
-		
-		if (!STATUS.OK.equals(status)) return status;
-		
-		return isBarcodeOK((FormDataObjIFace) dataObj);
-	}
-
-	@Override
-	public STATUS processBusinessRules(Object parentDataObj, Object dataObj, boolean isExistingObject)
-	{
-		// TODO Auto-generated method stub
-		return super.processBusinessRules(parentDataObj, dataObj, isExistingObject);
-	}
-
-	@Override
-	public Object processSearchObject(Object newParentDataObj, Object dataObjectFromSearch)
-	{
-		// TODO Auto-generated method stub
-		return super.processSearchObject(newParentDataObj, dataObjectFromSearch);
-	}
-
-	@Override
-	public void setObjectIdentity(Object dataObj, DraggableRecordIdentifier draggableIcon)
-	{
-		// TODO Auto-generated method stub
-		super.setObjectIdentity(dataObj, draggableIcon);
-	}
-
-	@Override
-	public boolean shouldCloneField(String fieldName)
-	{
-		if ("preparation".equals(fieldName)) return true;
-		
-		return super.shouldCloneField(fieldName);
-	}
-
-	@Override
-	public boolean shouldCreateSubViewData(String fieldName)
-	{
-		// TODO Auto-generated method stub
-		return super.shouldCreateSubViewData(fieldName);
-	}
+    static String formatToUI(Class<?> dataClass, String fieldName, String value)
+    {
+        DBTableInfo tableInfo = DBTableIdMgr.getInstance().getByClassName(dataClass.getName());
+        DBFieldInfo fieldInfo = tableInfo.getFieldByName(fieldName);
+        
+        String formattedField = "";
+        if (fieldInfo != null && fieldInfo.getFormatter() != null && value != null)
+        {
+            Object fmtObj = fieldInfo.getFormatter().formatToUI(value);
+            if (fmtObj != null)
+            {
+                formattedField = fmtObj.toString();
+            }
+        }
+        
+        return formattedField;
+    }
     
-    private void saveObject(final DataModelObjBase obj, final DataProviderSessionIFace session)
+    static DataModelObjBase saveObject(final DataModelObjBase obj, final DataProviderSessionIFace session)
     {
         if (obj != null)
         {
@@ -418,7 +214,7 @@ public class HUHFragmentBusRules extends BaseBusRules implements BusinessRulesIF
             {
                 if (obj.getId() != null)
                 {
-                    session.merge(obj);
+                    return session.merge(obj);
                 }
                 else
                 {
@@ -432,5 +228,16 @@ public class HUHFragmentBusRules extends BaseBusRules implements BusinessRulesIF
                 edu.ku.brc.exceptions.ExceptionTracker.getInstance().capture(HUHFragmentBusRules.class, ex);
             }
         }
+        return obj;
     }
+    
+	@Override
+	public STATUS processBusinessRules(Object dataObj)
+	{
+		STATUS status = super.processBusinessRules(dataObj);
+		
+		if (!STATUS.OK.equals(status)) return status;
+		
+		return isBarcodeOK((FormDataObjIFace) dataObj);
+	}
 }
