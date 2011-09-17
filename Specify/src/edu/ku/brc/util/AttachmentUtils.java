678bf726ab222448df05f15c5ae57eb4a87f6ca2
Maureen Kelly <mkelly>
2011-09-17 20:25:13 +0000

    changes to edu.ku.brc.util package for Specify-HUH merge with Specify-6.3 (part 17 of several)

---
+++ Specify/src/edu/ku/brc/util/AttachmentUtils.java
@@ -0,0 +1,277 @@
+/* Copyright (C) 2009, University of Kansas Center for Research
+ * 
+ * Specify Software Project, specify@ku.edu, Biodiversity Institute,
+ * 1345 Jayhawk Boulevard, Lawrence, Kansas, 66045, USA
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+package edu.ku.brc.util;
+
+import java.awt.Desktop;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URI;
+
+import javax.activation.FileTypeMap;
+import javax.activation.MimetypesFileTypeMap;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+
+import edu.ku.brc.specify.datamodel.Attachment;
+import edu.ku.brc.specify.datamodel.ObjectAttachmentIFace;
+import edu.ku.brc.ui.UIRegistry;
+import edu.ku.brc.util.thumbnails.Thumbnailer;
+
+/**
+ * Static class that is the singleton for the Attachment Manger.
+ * @code_status Complete
+ * 
+ * @author jstewart
+ * @author rods
+ */
+public class AttachmentUtils
+{
+    private static final Logger           log              = Logger.getLogger(AttachmentUtils.class);
+    private static AttachmentManagerIface attachMgr;
+    private static Thumbnailer            thumbnailer;
+    
+    /**
+     * @return the manager
+     */
+    public static AttachmentManagerIface getAttachmentManager()
+    {
+        return attachMgr;
+    }
+
+    /**
+     * @param mgr sets the manager
+     */
+    public static void setAttachmentManager(final AttachmentManagerIface mgr)
+    {
+        attachMgr = mgr;
+    }
+    
+    /**
+     * @param thumber sets the thumbnailer
+     */
+    public static void setThumbnailer(Thumbnailer thumber)
+    {
+        thumbnailer = thumber;
+    }
+    
+    /**
+     * The location of the directory in preferences may not exist.
+     * 
+     * @return whether there is an attachment manager.
+     */
+    public static boolean isAvailable()
+    {
+        return attachMgr != null;
+    }
+    
+    /**
+     * @return thumbnailer
+     */
+    public static Thumbnailer getThumbnailer()
+    {
+        return thumbnailer;
+    }
+    
+    /**
+     * @return whether the AttachmentManger can be used.
+     */
+    protected static boolean isAttachLocOK()
+    {
+        if (attachMgr != null)
+        {
+            return true;
+            
+        } else
+        {
+            UIRegistry.showLocalizedError("AttachmentUtils.NOT_AVAIL");
+        }
+        return false;
+    }
+    
+    /**
+     * @param attachmentLocation
+     * @return
+     */
+    public static boolean isAttachmentDirMounted(final File attachmentLocation)
+    {
+        String fullPath = "";
+        String statsMsg = "The test to write to the AttachmentLocation [%s] %s.";
+        try
+        {
+            fullPath = attachmentLocation.getCanonicalPath();
+            
+            if (attachmentLocation.exists())
+            {
+                if (attachmentLocation.isDirectory())
+                {
+                    File tmpFile = new File(attachmentLocation.getAbsoluteFile() + File.separator + System.currentTimeMillis() + System.getProperty("user.name"));
+                    //log.debug(String.format("Trying to write a file to AttachmentLocation [%s]", tmpFile.getCanonicalPath()));
+                    if (tmpFile.createNewFile())
+                    {
+                        // I don't think I need this anymore
+                        FileOutputStream fos = FileUtils.openOutputStream(tmpFile);
+                        fos.write(1);
+                        fos.close();
+                        tmpFile.delete();
+                        
+                        //log.debug(String.format(statsMsg, fullPath, "succeeded"));
+                        
+                        return true;
+                        
+                    } else
+                    {
+                        log.error(String.format("The Attachment Location [%s] atachment file couldn't be created", fullPath));
+                    }
+                } else
+                {
+                    log.error(String.format("The Attachment Location [%s] is not a directory.", fullPath));
+                }
+            } else
+            {
+                log.error(String.format("The Attachment Location [%s] doesn't exist.", fullPath));
+            }
+            
+        } catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        
+        log.debug(String.format(statsMsg, fullPath, "failed"));
+        
+        return false;
+    }
+    
+    /**
+     * @return the actionlistener for when things need to be displayed
+     */
+    public static ActionListener getAttachmentDisplayer()
+    {
+        ActionListener displayer = new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                Object source = e.getSource();
+                if (!(source instanceof Attachment) && !(source instanceof ObjectAttachmentIFace<?>))
+                {
+                    throw new IllegalArgumentException("Passed object must be an Attachment or ObjectAttachmentIFace");
+                }
+                
+                Attachment attachment = (source instanceof Attachment) ? (Attachment)source : ((ObjectAttachmentIFace<?>)source).getAttachment();
+                File original = null;
+                if (attachment.getId() != null)
+                {
+                    if (isAttachLocOK())
+                    {
+                        original = attachMgr.getOriginal(attachment);
+                    } else
+                    {
+                        return;
+                    }
+                }
+                else
+                {
+                    String origFile = attachment.getOrigFilename();
+                    original = new File(origFile);
+                }
+
+                String errMsg = null;
+                if (original != null && original.exists())
+                {
+                    try
+                    {
+                        Desktop.getDesktop().open(original);
+                        
+                    } catch (java.io.IOException ex)
+                    {
+                        errMsg = ex.getMessage();
+                        ex.printStackTrace();
+                    }
+                } else
+                {
+                    errMsg = attachMgr.getDirectory() != null ? attachMgr.getDirectory().getAbsolutePath() : "N/A";
+                }
+                
+                if (errMsg != null)
+                {
+                    UIRegistry.showLocalizedMsg("AttachmentUtils.ANF_TITLE", "AttachmentUtils.ANF", errMsg);
+                }
+            }
+        };
+        
+        return displayer;
+    }
+    
+    /**
+     * @param filename the file name to be checked
+     * @return the mime type
+     */
+    public static String getMimeType(final String filename)
+    {
+        if (filename == null)
+        {
+            return null;
+        }
+        
+        MimetypesFileTypeMap mimeMap = (MimetypesFileTypeMap)FileTypeMap.getDefaultFileTypeMap();
+        mimeMap.addMimeTypes("image/tif    tif");
+        mimeMap.addMimeTypes("image/tif    TIF");
+        mimeMap.addMimeTypes("image/png    png");
+        mimeMap.addMimeTypes("image/png    PNG");
+        mimeMap.addMimeTypes("application/vnd.google-earth.kml+xml kml");
+
+        return mimeMap.getContentType(filename);
+    }
+    
+    /**
+     * @param f the file to be opened
+     * @throws Exception
+     */
+    public static void openFile(final File f) throws Exception
+    {
+        Desktop.getDesktop().open(f);
+    }
+    
+    /**
+     * @param uri the uri to be opened
+     * @throws Exception
+     */
+    public static void openURI(final URI uri) throws Exception
+    {
+        Desktop.getDesktop().browse(uri);
+    }
+    
+    /**
+     * @param args app args
+     */
+    public static void main(String[] args)
+    {
+        String[] filenames = {"hello.txt","a.bmp","b.pdf","hello.gif","blha.tiff","c.jpg",null,"blah.kml"};
+    
+        for (String name: filenames)
+        {
+            System.out.println(AttachmentUtils.getMimeType(name));
+        }
+    }
+    
+
+}
---
+++ Specify/src/edu/ku/brc/util/FileStoreAttachmentManager.java
@@ -0,0 +1,324 @@
+/* Copyright (C) 2009, University of Kansas Center for Research
+ * 
+ * Specify Software Project, specify@ku.edu, Biodiversity Institute,
+ * 1345 Jayhawk Boulevard, Lawrence, Kansas, 66045, USA
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+package edu.ku.brc.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Vector;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+
+import edu.ku.brc.specify.datamodel.Attachment;
+import edu.ku.brc.ui.UIRegistry;
+
+/**
+ * An implementation of AttachmentManagerIface that uses the underlying filesystem to
+ * provide storage of the attachments and thumbnails.
+ *
+ * @author jstewart
+ * @author rods
+ * @code_status Beta
+ */
+public class FileStoreAttachmentManager implements AttachmentManagerIface
+{
+    private static final Logger  log   = Logger.getLogger(FileStoreAttachmentManager.class);
+    
+    private static final String ORIGINAL   = "originals";
+    private static final String THUMBNAILS = "thumbnails";
+    
+    
+    /** The base directory path of where the files will be stored. */
+    protected String baseDirectory;
+    
+    /** The directory inside the base that will store the original files. */
+    protected File originalsDir;
+    
+    /** The directory inside the base that will store the thumbnail files. */
+    protected File thumbsDir;
+    
+    /** 
+     * A collection of all files created by calls to setStorageLocationIntoAttachment
+     * that have not yet been filled by calls to storeAttachmentFile.
+     */
+    protected Vector<String> unfilledFiles;
+    
+    /**
+     * Creates a new instance, setting baseDirectory to null.
+     * @throws IOException if either of the storage directories is not writable
+     */
+    public FileStoreAttachmentManager(final File baseDirectory) throws IOException
+    {
+        setDirectory(baseDirectory);
+    }
+    
+    /**
+     * @param baseDir
+     * @throws IOException
+     */
+    public void setDirectory(final File baseDir) throws IOException
+    {
+        this.baseDirectory = baseDir.getAbsolutePath();
+        this.originalsDir  = new File(baseDirectory + File.separator + ORIGINAL);
+        this.thumbsDir     = new File(baseDirectory + File.separator + THUMBNAILS);
+        
+        // create the directories, if they don't already exist
+        if (!originalsDir.exists() && !originalsDir.mkdirs())
+        {
+            log.error("setDirectory - failed to create originals["+originalsDir.getAbsolutePath()+"]");
+        }
+        
+        if (!thumbsDir.exists() && !thumbsDir.mkdirs())
+        {
+            log.error("setDirectory - failed to create thumbsDir["+thumbsDir.getAbsolutePath()+"]");
+        }
+        
+        // make sure the directories are writable
+        if (!originalsDir.canWrite())
+        {
+            throw new IOException("Storage directory not writable: " + originalsDir.getAbsolutePath());
+        }
+        if (!thumbsDir.canWrite())
+        {
+            throw new IOException("Storage directory not writable: " + originalsDir.getAbsolutePath());
+        }
+        
+        unfilledFiles = new Vector<String>();
+    }
+    
+    /* (non-Javadoc)
+     * @see edu.ku.brc.util.AttachmentManagerIface#getDirectory()
+     */
+    @Override
+    public File getDirectory()
+    {
+        return new File(this.baseDirectory);
+    }
+
+    /* (non-Javadoc)
+     * @see edu.ku.brc.util.AttachmentManagerIface#setStorageLocationIntoAttachment(edu.ku.brc.specify.datamodel.Attachment, boolean)
+     */
+    @Override
+    public boolean setStorageLocationIntoAttachment(final Attachment attachment, final boolean doDisplayErrors)
+    {
+        String attName    = attachment.getOrigFilename();
+        int    lastPeriod = attName.lastIndexOf('.');
+        String suffix     = ".att";
+        
+        if (lastPeriod != -1)
+        {
+            // Make sure the file extension (if any) remains the same so the host
+            // filesystem still sees the files as the proper types.  This is simply
+            // to make the files browsable from a system file browser.
+            suffix = ".att" + attName.substring(lastPeriod);
+        }
+        
+        String errMsg          = null;
+        String storageFilename = "";
+        try
+        {
+            if (originalsDir == null || !originalsDir.exists())
+            {
+                errMsg = UIRegistry.getLocalizedMessage("ATTCH_STRG_DIR_ERR", (originalsDir != null ? originalsDir.getAbsolutePath() : "(missing dir name)"));
+                log.error("originalsDir doesn't exist["+(originalsDir != null ? originalsDir.getAbsolutePath() : "null")+"]");
+            }
+            
+            // find an unused filename in the originals dir
+            File storageFile = File.createTempFile("sp6-", suffix, originalsDir);
+            if (storageFile.exists())
+            {
+                attachment.setAttachmentLocation(storageFile.getName());
+                unfilledFiles.add(attachment.getAttachmentLocation());
+                return true;
+            }
+            errMsg = UIRegistry.getLocalizedMessage("ATTCH_NOT_SAVED_REPOS", (storageFile != null ? storageFile.getAbsolutePath() : "(missing file name)"));
+            log.error("storageFile doesn't exist["+(storageFile != null ? storageFile.getAbsolutePath() : "null")+"]");
+        }
+        catch (IOException e)
+        {
+            if (doDisplayErrors)
+            {
+                errMsg = UIRegistry.getLocalizedMessage("ATTCH_NOT_SAVED_REPOS", storageFilename);
+                return false;
+            }
+            
+            // This happens when errors are not displayed.
+            e.printStackTrace();
+            edu.ku.brc.af.core.UsageTracker.incrHandledUsageCount();
+            edu.ku.brc.exceptions.ExceptionTracker.getInstance().capture(FileStoreAttachmentManager.class, e);
+        }
+        
+        if (doDisplayErrors && errMsg != null)
+        {
+            UIRegistry.showError(errMsg);
+        }
+        return false;
+    }
+
+    /* (non-Javadoc)
+     * @see edu.ku.brc.util.AttachmentManagerIface#getOriginal(edu.ku.brc.specify.datamodel.Attachment)
+     */
+    public File getOriginal(final Attachment attachment)
+    {
+        String fileLoc = attachment.getAttachmentLocation();
+        if (StringUtils.isNotEmpty(fileLoc))
+        {
+            File storedFile = new File(baseDirectory + File.separator + ORIGINAL + File.separator + fileLoc);
+            if (storedFile.exists())
+            {
+                return storedFile;
+            }
+        } else
+        {
+            log.error("AttachmentLocation is null for id["+attachment.getId()+"]");
+        }
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see edu.ku.brc.util.AttachmentManagerIface#getThumbnail(edu.ku.brc.specify.datamodel.Attachment)
+     */
+    public File getThumbnail(final Attachment attachment)
+    {
+        String fileLoc = attachment.getAttachmentLocation();
+        if (StringUtils.isNotEmpty(fileLoc))
+        {
+            File storedFile = new File(baseDirectory + File.separator + THUMBNAILS + File.separator + fileLoc);
+            if (storedFile.exists())
+            {
+                return storedFile;
+            }
+        } else
+        {
+            log.error("AttachmentLocation is null for id["+attachment.getId()+"]");
+        }
+        return null;
+    }
+
+
+    /* (non-Javadoc)
+     * @see edu.ku.brc.util.AttachmentManagerIface#storeAttachmentFile(edu.ku.brc.specify.datamodel.Attachment, java.io.File, java.io.File)
+     */
+    public void storeAttachmentFile(final Attachment attachment, final File attachmentFile, final File thumbnail) throws IOException
+    {
+        // copy the original into the storage system
+        String attachLoc      = attachment.getAttachmentLocation();
+        File   repositoryFile = new File(baseDirectory + File.separator + ORIGINAL + File.separator + attachLoc);
+        
+        // Copy it to the Repository
+        FileUtils.copyFile(attachmentFile, repositoryFile);
+        
+        // copy the thumbnail, if any, into the storage system
+        if (thumbnail != null)
+        {
+            File thumbFile = new File(baseDirectory + File.separator + THUMBNAILS + File.separator + attachLoc);
+            FileUtils.copyFile(thumbnail, thumbFile);
+        }
+        
+        // since we have now made use of the temp file we created earlier, we don't
+        // need to keep track of it as an 'unfilled' file to be cleaned up later
+        unfilledFiles.remove(attachment.getAttachmentLocation());
+    }
+
+    /* (non-Javadoc)
+     * @see edu.ku.brc.util.AttachmentManagerIface#replaceOriginal(edu.ku.brc.specify.datamodel.Attachment, java.io.File)
+     */
+    public void replaceOriginal(final Attachment attachment, final File newOriginal, final File newThumbnail) throws IOException
+    {
+        String attachLoc = attachment.getAttachmentLocation();
+        File origFile = new File(baseDirectory + File.separator + ORIGINAL + File.separator + attachLoc);
+        File thumbFile = new File(baseDirectory + File.separator + THUMBNAILS + File.separator + attachLoc);
+
+        replaceFile(origFile, newOriginal);
+        replaceFile(thumbFile, newThumbnail);
+    }
+    
+    /**
+     * Replace origFile with newFile, attempting to recover from an exception if it occurs.
+     * 
+     * @param origFile the original file
+     * @param newFile the replacement version
+     * @throws IOException a disk IO error occurred during the process
+     */
+    protected void replaceFile(final File origFile, final File newFile) throws IOException
+    {
+        File tmpOrig = File.createTempFile("sp6-", ".tmp");
+        FileUtils.copyFile(origFile, tmpOrig);
+        try
+        {
+            // try to copy the new attachment into place
+            // if this fails, we should try to copy the old original back into place
+            FileUtils.copyFile(newFile, origFile);
+        }
+        catch (IOException e)
+        {
+            edu.ku.brc.af.core.UsageTracker.incrHandledUsageCount();
+            edu.ku.brc.exceptions.ExceptionTracker.getInstance().capture(FileStoreAttachmentManager.class, e);
+            // if the attachment file differs from the original that we copied to a tmp location...
+            if( !FileUtils.contentEquals(origFile, tmpOrig) )
+            {
+                // copy the tmp version back into place
+                FileUtils.copyFile(tmpOrig, origFile);
+            }
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see edu.ku.brc.util.AttachmentManagerIface#deleteAttachmentFiles(edu.ku.brc.specify.datamodel.Attachment)
+     */
+    public void deleteAttachmentFiles(final Attachment attachment) throws IOException
+    {
+        String attachLoc = attachment.getAttachmentLocation();
+        if (StringUtils.isNotEmpty(attachLoc))
+        {
+            File   origFile  = new File(baseDirectory + File.separator + ORIGINAL + File.separator + attachLoc);
+            File   thumbFile = new File(baseDirectory + File.separator + THUMBNAILS + File.separator + attachLoc);
+            
+            if (origFile.exists())
+            {
+                origFile.delete();
+            }
+            
+            if (thumbFile.exists())
+            {
+                thumbFile.delete();
+            }
+            
+        } else
+        {
+            log.error("The AttachmentLocation was null/empty for attachment id: "+attachment.getId());
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see edu.ku.brc.util.AttachmentManagerIface#cleanup()
+     */
+    public void cleanup()
+    {
+        // delete all of the temp files we created that were never used for storing
+        // attachment originals
+        for (String unusedFile: unfilledFiles)
+        {
+            File f = new File(originalsDir.getAbsolutePath() + File.separator + unusedFile);
+            f.delete();
+        }
+    }
+}
