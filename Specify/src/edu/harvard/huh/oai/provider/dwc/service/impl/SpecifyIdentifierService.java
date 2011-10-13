db3aca69d12f503729159696b1d321fa629a81af
Maureen Kelly <mkelly>
2011-10-13 20:14:14 +0000

    initial implementation of createOaiIdentifier and extractInternalIdentifier

--- Specify/src/edu/harvard/huh/oai/provider/dwc/service/impl/SpecifyIdentifierService.java
+++ Specify/src/edu/harvard/huh/oai/provider/dwc/service/impl/SpecifyIdentifierService.java
@@ -1,18 +1,23 @@
 package edu.harvard.huh.oai.provider.dwc.service.impl;
 
+import edu.ku.brc.specify.datamodel.CollectionObject;
 import au.org.tern.ecoinformatics.oai.provider.service.IdentifierService;
 
 public class SpecifyIdentifierService implements IdentifierService {
 
 	@Override
-	public String createOaiIdentifier(Object arg0) {
-		// TODO Auto-generated method stub
+	public String createOaiIdentifier(Object object) {
+		if (object instanceof CollectionObject) {
+			return "co" + ((CollectionObject) object).getId();
+		}
 		return null;
 	}
 
 	@Override
-	public String extractInternalIdentifier(String arg0) {
-		// TODO Auto-generated method stub
+	public String extractInternalIdentifier(String string) {
+		if (string.startsWith("co")) {
+			return string.substring(2);
+		}
 		return null;
 	}
 
