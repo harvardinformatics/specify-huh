253fa9656f1224906ec737aac574a7a0e71e2f39
Maureen Kelly <mkelly>
2011-10-13 20:16:42 +0000

    a fuller implementation of listIdentifiers

--- Specify/src/edu/harvard/huh/oai/provider/dwc/service/impl/DwcOaiPmhService.java
+++ Specify/src/edu/harvard/huh/oai/provider/dwc/service/impl/DwcOaiPmhService.java
@@ -1,34 +1,48 @@
 package edu.harvard.huh.oai.provider.dwc.service.impl;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import ORG.oclc.oai.server.catalog.RecordFactory;
 import ORG.oclc.oai.server.verb.CannotDisseminateFormatException;
+import au.org.tern.ecoinformatics.oai.provider.model.IdentifierRecord;
 import au.org.tern.ecoinformatics.oai.provider.model.ListIdentifiersResponse;
 import au.org.tern.ecoinformatics.oai.provider.model.Record;
-import au.org.tern.ecoinformatics.oai.provider.service.IdentifierService;
 import au.org.tern.ecoinformatics.oai.provider.service.NativeObjectService;
 import au.org.tern.ecoinformatics.oai.provider.service.OaiPmhService;
 
 public class DwcOaiPmhService implements OaiPmhService {
 
-	private String dateFormat;
+	private String dateFormatString;
+	private SimpleDateFormat dateFormat;
 	private List<String> metadataFormats;
 	private List<String> sets;
 	
 	private RecordFactory recordFactory;
-	private IdentifierService identifierService;
 	private NativeObjectService nativeObjectService;
 	
 	public DwcOaiPmhService() {
+		this.dateFormatString = "YYYY-MM-dd";
+		this.dateFormat = new SimpleDateFormat(dateFormatString);
 		this.metadataFormats = new ArrayList<String>();
+		this.sets = new ArrayList<String>();
+	}
+
+	
+	public void setNativeObjectService(NativeObjectService nativeObjectService) {
+		this.nativeObjectService = nativeObjectService;
+	}
+	
+	public void setRecordFactory(RecordFactory recordFactory) {
+		this.recordFactory = recordFactory;
 	}
 
 	@Override
 	public String getDateFormat() {
-		return dateFormat;
+		return dateFormatString;
 	}
 
 	@Override
@@ -39,7 +53,7 @@ public class DwcOaiPmhService implements OaiPmhService {
 	@Override
 	public Record getRecord(String identifier, String metadataPrefix) {
 
-		String internalIdentifier = getIdentifierService().extractInternalIdentifier(identifier);
+		String internalIdentifier = getRecordFactory().fromOAIIdentifier(identifier);
 
 		Long id = null;
 		try {
@@ -50,13 +64,14 @@ public class DwcOaiPmhService implements OaiPmhService {
 		}
 
 		Object nativeObject = getNativeObjectService().getObject(id);
+		if (nativeObject == null) return null;
 
 		String schemaUrl = null; // TODO: find schema url for metadata prefix
 		
 		String datestamp = getRecordFactory().getDatestamp(nativeObject);
 		boolean deleted = getRecordFactory().isDeleted(nativeObject);
 
-		String setSpec = getIdentifierService().extractSetSpec(identifier);
+		String setSpec = null; // TODO: implement sets?
 		
 		String namespacedRecordXml = null;
 		try {
@@ -91,13 +106,26 @@ public class DwcOaiPmhService implements OaiPmhService {
 		if (!getMetadataFormats().contains(metadataPrefix)) {
 			return null;
 		}
-		List<Object> nativeObjects = getNativeObjectService().getObjects(from, until);
-		
-		return null;
-	}
 
-	private IdentifierService getIdentifierService() {
-		return identifierService;
+		List<IdentifierRecord> identifierRecords = new ArrayList<IdentifierRecord>();
+		
+		for (Object nativeObject : getNativeObjectService().getObjects(from, until)) {
+
+			String identifier = getRecordFactory().getOAIIdentifier(nativeObject);
+			Date date = parseDate(getRecordFactory().getDatestamp(nativeObject));
+			
+			IdentifierRecord identifierRecord = new IdentifierRecord();
+			identifierRecord.setDate(date);
+			identifierRecord.setIdentifier(identifier);
+			identifierRecord.setDeleted(false);
+			
+			identifierRecords.add(identifierRecord);
+		}
+		
+		ListIdentifiersResponse response = new ListIdentifiersResponse();
+		response.setIdentifierRecords(identifierRecords);
+
+		return response;
 	}
 	
 	private NativeObjectService getNativeObjectService() {
@@ -106,5 +134,15 @@ public class DwcOaiPmhService implements OaiPmhService {
 	
 	private RecordFactory getRecordFactory() {
 		return recordFactory;
+	}
+	
+	private Date parseDate(String date) {
+		try {
+			return dateFormat.parse(date);
+		}
+		catch (ParseException e) {
+			// TODO Auto-generated catch block
+			return null;
+		}
 	}
 }
