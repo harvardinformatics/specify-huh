97e037ab049f7443f755c0b1f036ed59be67dc48
lowery <lowery>
2011-11-30 18:39:05 +0000

    ISSUE: tree unit testing PURPOSE: Added BaseTest and BaseTreeableTest for use as superclasses DESCRIPTION: Tests should be derived from these classes, contains  hibernate session initialization code common to all test cases

--- Specify/src/edu/harvard/huh/specify/tests/BaseTest.java
+++ Specify/src/edu/harvard/huh/specify/tests/BaseTest.java
@@ -1,65 +1,56 @@
+/*
+ * Created on Nov 30, 2011
+ *
+ * Copyright © 2011 President and Fellows of Harvard College
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * @Author: David B. Lowery  lowery@cs.umb.edu
+ */
+
 package edu.harvard.huh.specify.tests;
 
-import static org.junit.Assert.*;
+import java.util.Properties;
 
 import org.hibernate.Session;
-import org.junit.Test;
 
 import edu.ku.brc.dbsupport.DBConnection;
 import edu.ku.brc.dbsupport.HibernateUtil;
-import edu.ku.brc.specify.Specify;
-import edu.ku.brc.specify.datamodel.busrules.TaxonBusRules;
 
-/**
- * Tests the miscellaneous changed methods in the tree by parent upgrade. Tests
- * the recursive query replacements for the original S/HQL statements.
- * 
- * @author lchan
- * 
- */
 public class BaseTest {
+	protected Session session = null;
+	
+	protected Session getSession() {
+		try {
+			Properties props = new Properties();
+			props.load(getClass().getResourceAsStream("testing.properties"));
+		
+			DBConnection dbConn = DBConnection.getInstance();
+		
+			dbConn.setDriver(props.getProperty("testing.db.driver"));
+			dbConn.setDialect(props.getProperty("testing.db.dialect"));
+			dbConn.setDatabaseName(props.getProperty("testing.db.name"));
+			dbConn.setConnectionStr(props.getProperty("testing.db.connstr"));
+			dbConn.setUsernamePassword(props.getProperty("testing.db.username"),
+					props.getProperty("testing.db.password"));
+		
+			session = HibernateUtil.getSessionFactory().openSession();
+		} catch (Exception e) {
+			e.printStackTrace();
 
-    protected Session getSession() {
-        DBConnection dbc = DBConnection.getInstance();
-        dbc.setConnectionStr("jdbc:mysql://127.0.0.1:3307/specify?characterEncoding=UTF-8&autoReconnect=true");
-        dbc.setDatabaseName("specify");
-        dbc.setDialect("org.hibernate.dialect.MySQLDialect");
-        dbc.setDriver("com.mysql.jdbc.Driver");
-        dbc.setServerName("127.0.0.1");
-        dbc.setUsernamePassword("lchan", "1579ASDF");
-
-        Specify.setUpSystemProperties();
-
-        Session session = HibernateUtil.getNewSession();
-        
-        return session;
-    }
-
-
-
-    @Test
-    public void testErtiSetup() {
-
-    }
-
-    @Test
-    public void testGetNodeNumberCriteria() {
-
-    }
-
-    @Test
-    public void testHibernateTreeDataServiceImplCreateNode() {
-
-    }
-
-    @Test
-    public void testGetDescendantCount() {
-
-    }
-
-    @Test
-    public void testTreeTableViewerCreateNode() {
-
-    }
-
+		}
+		return session;
+	}
 }
---
+++ Specify/src/edu/harvard/huh/specify/tests/BaseTreeableTest.java
@@ -0,0 +1,110 @@
+/*
+ * Created on Nov 21, 2011
+ *
+ * Copyright © 2011 President and Fellows of Harvard College
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * @Author: David B. Lowery  lowery@cs.umb.edu
+ */
+
+package edu.harvard.huh.specify.tests;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Properties;
+import java.util.Queue;
+import java.util.Set;
+
+import javax.swing.UIManager;
+
+import org.hibernate.Session;
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.ku.brc.dbsupport.DBConnection;
+import edu.ku.brc.dbsupport.HibernateUtil;
+import edu.ku.brc.specify.datamodel.TreeDefIface;
+import edu.ku.brc.specify.datamodel.Treeable;
+import edu.ku.brc.specify.treeutils.HibernateTreeDataServiceImpl;
+import edu.ku.brc.specify.treeutils.TreeDataService;
+
+public class BaseTreeableTest extends BaseTest {
+	protected  Class treeDefClass;
+	protected  TreeDefIface currentDef;
+	protected  TreeDataService treeService;
+	protected  Treeable root;
+	protected  final int DEFAULT_TREE_DEF_ID = 1;
+
+	public void initialize(Class treeDefClass) {
+		getSession();
+		this.treeDefClass = treeDefClass;
+		currentDef = (TreeDefIface)session.load(treeDefClass, DEFAULT_TREE_DEF_ID);
+		treeService = new HibernateTreeDataServiceImpl();
+		session.close();
+
+		root = treeService.getRootNode(currentDef);
+	}
+
+	@Test public void testRootNode() 		
+	{
+		assertTrue(treeService.getRootNode(currentDef) != null);
+	}
+	@Test public void testRootNodeChildren() {
+		assertTrue(treeService.getChildNodes(root) != null);
+	}
+
+	@Test public void checkTreeIntegrity() {	
+		Queue<Treeable> nodes = new LinkedList<Treeable>();
+		int count = 0;
+		Set<Integer> visited = new HashSet<Integer>();
+		boolean error = false;
+		String errStr = "No Errors!";
+		nodes.offer(root);
+
+		while (!error && !nodes.isEmpty()) {
+			Treeable parent = nodes.poll();
+			for (Treeable node : (Set<Treeable>)treeService.getChildNodes(parent)) {
+
+				// First, check the parent object reference and parent id
+				if (node.getParent() != null) {
+					if(node.getParent().getTreeId() != parent.getTreeId()) {
+						errStr = node.getTreeId() + ": " + node.getFullName() + "(The child does not point back to the correct parent!)";
+						error = true;
+					}
+				} else {
+					errStr = node.getTreeId() + ": " + node.getFullName() + "(This node has no parent and isn't the root!)";
+					error = true;
+				}
+				// Second, check for cycles by checking if this node has been visited more than once
+				int id = node.getTreeId();
+				if (visited.contains(id)) {
+					errStr = node.getTreeId() + ": " + node.getFullName() + "This node has been visited already!";
+					error = true;
+				}
+
+				// Enqueue current node and increment the count
+				nodes.offer(node);
+				visited.add(id);
+				count++;
+			}
+		}
+		assertFalse(error);
+		System.out.println(errStr);
+	}
+}
