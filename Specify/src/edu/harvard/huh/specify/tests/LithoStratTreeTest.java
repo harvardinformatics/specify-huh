5df0d2135bf782f34f5ba04298dc723171a12f76
lowery <lowery>
2011-11-30 18:44:23 +0000

    ISSUE: tree junit tests PURPOSE: added junit tests that extend BaseTreeableTest DESCRIPTION: tests for all the implementing classes of Treeable interface. Add more tests that are tailored to each set of data later.

---
+++ Specify/src/edu/harvard/huh/specify/tests/LithoStratTreeTest.java
@@ -0,0 +1,33 @@
+/*
+ * Created on Nov 30, 2011
+ *
+ * Copyright © 2011 President and Fellows of Harvard College
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * @Author: David B. Lowery  lowery@cs.umb.edu
+ */
+
+package edu.harvard.huh.specify.tests;
+
+import org.junit.Before;
+
+import edu.ku.brc.specify.datamodel.LithoStratTreeDef;
+
+public class LithoStratTreeTest extends BaseTreeableTest {
+	@Before public void setTreeable() {
+		initialize(new LithoStratTreeDef().getClass());
+	}
+}
