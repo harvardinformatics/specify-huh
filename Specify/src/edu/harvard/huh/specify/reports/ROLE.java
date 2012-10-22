466f1273542c97786b76f9493ce1add854bcdb98
Maureen Kelly <mkelly>
2012-10-22 19:30:22 +0000

    copied enum edu.harvard.huh.asa.Transaction.ROLE to its own class in edu.harvard.huh.specify.reports so that it can be used in builds for releases, which omit the asa package; updated references to it in the reports package

---
+++ Specify/src/edu/harvard/huh/specify/reports/ROLE.java
@@ -0,0 +1,7 @@
+package edu.harvard.huh.specify.reports;
+
+public enum ROLE {
+	// see edu.harvard.huh.asa.Transaction.ROLE
+	// (not imported here because we omit this package from the build for the releases)
+	Borrower, Benefactor, Collector, Contact, Contributor, Donor, ForUseBy, Guest, Lender, Other, Preparer, Receiver, Reviewer, Seller, Sponsor, Staff, Student	
+}
--- Specify/src/edu/harvard/huh/specify/reports/ReportAccession.java
+++ Specify/src/edu/harvard/huh/specify/reports/ReportAccession.java
@@ -30,7 +30,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import edu.harvard.huh.asa.Transaction.ROLE;
 import edu.ku.brc.specify.datamodel.Accession;
 import edu.ku.brc.specify.datamodel.AccessionAgent;
 import edu.ku.brc.specify.datamodel.AccessionPreparation;
